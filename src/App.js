b014fbcbb0a48a1a9a7ded56691341a11209f004
Nat Welch <nat@natwelch.com>
2017-08-23 02:03:29 +0000

    Chugging along

--- src/App.js
+++ src/App.js
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'tachyons-components'
 
 class App extends Component {
   render() {
+    const Button = styled('button')`
+  f6 f5-ns fw6 dib ba
+  b--black-20 bg-blue white
+  ph3 ph4-ns pv2 pv3-ns br2
+  grow no-underline
+`
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Button mr2>
+      Hello
+      </Button>
     );
   }
 }
