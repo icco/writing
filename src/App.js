7d265725329609b834a11dded9d5f1b5089ee3b7
Nat Welch <nat@natwelch.com>
2017-08-23 12:32:31 +0000

    title tweak

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
       <div className="App-header">
-      <h2>My blog</h2>
+      <h2>Nat? Nat. Nat!</h2>
       </div>
       <div>
       {this.props.children}
