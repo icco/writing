cd9ef7f1238d963bb9c04334e04a85a408cf02ec
Nat Welch <nat@natwelch.com>
2017-08-23 02:39:24 +0000

    Basics

--- src/Post.js
+++ src/Post.js
@@ -1,12 +1,45 @@
 import React, { Component } from 'react';
 
 class Post extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loaded: false
+    };
+  }
+
+  componentWillMount() {
+    let slug = this.props.params.slug;
+
+    fetch(`https://writing.natwelch.com/post/${slug}/json`)
+      .then((response) => response.json())
+      .then((resp) => {
+        console.log(resp);
+        this.setState({
+          loaded: true,
+          post: resp
+        })
+      });
+  }
+
   render() {
-    return (
-      <div>
-      Post
-      </div>
-    );
+    if (this.state.loaded) {
+      const post = this.state.post;
+
+      return (
+        <div>
+          <h1>{post.title}</h1>
+          <div dangerouslySetInnerHTML={{__html: post.body}} />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }
   }
 }
 
