3898fb23d85e781a8ff686e13219561649d0e7fa
Nat Welch <nat@natwelch.com>
2017-08-29 02:24:21 +0000

    hashtag progress

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router'
 import './App.css';
 import styled from 'tachyons-components'
 
-const Header = styled('header')`pv1 ph3 pa4-m pa5-l oh pos-rel mt0-ns mt4`
+const Header = styled('header')`dt w-100`
+const SubHeader = styled('div')`dtc w-50 v-mid`
+const Title = styled('h1')`mb2 mt0 tracked tl-ns tc w-100`
+const TitleBox = styled('div')`dib v-mid pa0 w-100 w-auto-ns`
+const HeaderImg = styled('img')`v-mid mh0-ns dib-ns db center ph0 w4`
+const TitleLink = styled(Link)`no-underline black`
 
 class App extends Component {
   render() {
     return (
       <div>
-      <Header>
-      <div class="dt w-100">
-      <div class="dtc w-50 v-mid">
-      <a href="/"><img src="/img/dots.png" class="v-mid mh0-ns dib-ns db center ph0 w4" alt="Nat? Nat. Nat!"></img></a>
-      <div class="dib v-mid pa0 w-100 w-auto-ns">
-      <a href="/" class="no-underline black"><h1 class="mb2 mt0 tracked tl-ns tc w-100">Nat? Nat. Nat!</h1></a>
-      </div>
-      </div>
-      </div>
-      </Header>
+        <Header>
+          <SubHeader>
+            <Link to="/"><HeaderImg src="/img/dots.png" alt="Nat? Nat. Nat!"></HeaderImg></Link>
+            <TitleBox>
+              <TitleLink to="/"><Title>Nat? Nat. Nat!</Title></TitleLink>
+            </TitleBox>
+          </SubHeader>
+        </Header>
 
-
-      <div>
-      {this.props.children}
-      </div>
+        <div>
+          {this.props.children}
+        </div>
       </div>
     );
   }
