d1b68ca135d2f5fa195a6cf00f85c3d09f0df9aa
Nat Welch <nat@natwelch.com>
2017-10-15 18:42:40 +0000

    switch unreg

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
+'use strict';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
+import { unregister } from './registerServiceWorker';
 
 import Routes from './routes';
+
+unregister();
 
 ReactDOM.render(
  <Routes history={browserHistory} />,
