2f6705c414f9edf69a49b7a6fc86fa0f58c2b790
Nat Welch <nat@natwelch.com>
2017-10-18 20:37:57 +0000

    add to index

--- src/index.js
+++ src/index.js
@@ -4,11 +4,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { browserHistory } from 'react-router';
 import { unregister } from './registerServiceWorker';
-
+import ReactGA from 'react-ga';
 import Routes from './routes';
 
+ReactGA.initialize('UA-333449-17');
+
+function logPageView() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 ReactDOM.render(
- <Routes history={browserHistory} />,
+ <Routes history={browserHistory} onUpdate={logPageView} />,
  document.getElementById('root')
 );
 unregister();
