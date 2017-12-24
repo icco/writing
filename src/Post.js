87558065ad1042af042b78488319bc657b946144
Nat Welch <nat@natwelch.com>
2017-12-24 23:46:53 +0000

    progress

--- src/Post.js
+++ src/Post.js
@@ -1,59 +1,44 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Helmet } from "react-helmet";
 import { Link } from 'react-router'
 import Moment from 'react-moment';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
 
-class Post extends Component {
-  constructor(props) {
-    super(props);
+function PostComponent({ data: {Post, error, loading}}) {
+  if (loading) {
+    return <p>Loading...</p>;
+  } else if (error) {
+    console.log(error);
+    return <p>Error!</p>;
+  } else {
+    console.log(Post);
+    return (
+      <div>
+        <Helmet>
+          <title>{`Nat? Nat. Nat! #${Post.Id} - "${Post.Title}"`}</title>
+        </Helmet>
 
-    this.state = {
-      loaded: false
-    };
-  }
-
-  componentWillMount() {
-    let slug = this.props.params.slug;
-
-    fetch(`https://natwelch-writing.appspot.com/post/${slug}/json`)
-      .then((response) => response.json())
-      .then((resp) => {
-        this.setState({
-          loaded: true,
-          post: resp
-        })
-      });
-  }
-
-  render() {
-    if (this.state.loaded) {
-      const post = this.state.post;
-
-      return (
-        <div>
-          <Helmet>
-          <title>{`Nat? Nat. Nat! #${post.id} - "${post.title}"`}</title>
-          </Helmet>
-
-          <div className="vat mt3 mb0 dib w-100 mh0" key={post.id}>
-            <Link className="no-underline dim" to={`/post/${post.id}`}>
-              <h2 className="f2-ns f3 measure lh-title fw1 mt0 mb1">{post.title}</h2>
-            </Link>
-            <p className="mt0 f5-ns f6 db mb3"><Moment className="dib" interval={0} format="MMM. Do YYYY, h a z">{post.date}</Moment>. {Math.ceil(post.readtime / 60)} minute read.</p>
-            <div className="db lh-copy">
-              <div className="dib" dangerouslySetInnerHTML={{__html: post.html}}></div>
-            </div>
+        <div className="vat mt3 mb0 dib w-100 mh0" key={Post.Id}>
+          <Link className="no-underline dim" to={`/post/${Post.Id}`}>
+            <h2 className="f2-ns f3 measure lh-title fw1 mt0 mb1">{Post.Title}</h2>
+          </Link>
+          <p className="mt0 f5-ns f6 db mb3"><Moment className="dib" interval={0} format="MMM. Do YYYY, h a z">{Post.Datetime}</Moment>. {Math.ceil(Post.Readtime / 60)} minute read.</p>
+          <div className="db lh-copy">
+            <div className="dib" dangerouslySetInnerHTML={{__html: Post.Html}}></div>
           </div>
         </div>
-      );
-    } else {
-      return (
-        <div>
-          Loading...
-        </div>
-      );
-    }
+      </div>
+    );
   }
 }
 
-export default Post;
+export default graphql(gql`
+  query PostQuery {
+    Post(Id: 677) {
+      Id
+      Title
+      Datetime
+    }
+  }
+`)(PostComponent);
--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ import { Router, IndexRoute, Route } from 'react-router';
 import App from './App';
 import Home from './Home';
 import About from './About';
-import Post from './Post';
+import PostComponent from './Post';
 
 const Routes = (props) => (
   <Router {...props}>
   <Route path="/" component={App}>
   <IndexRoute component={Home} />
   <Route path="/p/:page" component={Home} />
-  <Route path="/post/:slug" component={Post} />
+  <Route path="/post/:slug" component={PostComponent} />
   <Route path="/about" component={About} />
   </Route>
   </Router>
