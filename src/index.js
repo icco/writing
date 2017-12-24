21965a8a9a8591396cb7c8d8c33e33aeb5713d0d
Nat Welch <nat@natwelch.com>
2017-12-24 16:42:16 +0000

    For future reference

--- src/index.js
+++ src/index.js
@@ -18,6 +18,7 @@ function logPageView() {
 
 const client = new ApolloClient({
   link: new HttpLink({uri: "https://writing-be.natwelch.com/graphql"}),
+  //link: new HttpLink({uri: "http://localhost:8080/graphql"}),
   cache: new InMemoryCache()
 });
 
