c3c7f3b68625d26115007e060f0f33bbf0bd07b3
Nat Welch <nat@natwelch.com>
2017-12-24 23:22:18 +0000

    Working home ~ish

--- src/Home.js
+++ src/Home.js
@@ -2,92 +2,47 @@
 
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import React, { Component } from 'react';
+import React from 'react';
 import { Helmet } from "react-helmet";
 import { Link } from 'react-router'
 import Moment from 'react-moment';
 import 'moment-timezone';
 import 'tachyons';
 
-class Home extends Component {
-
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      loaded: false
-    };
-  }
-
-  fetchPosts(page) {
-    // TODO: Add page support
-    fetch("https://natwelch-writing.appspot.com/summary.json")
-      .then((response) => response.json())
-      .then((resp) => {
-        console.debug(resp);
-        this.setState({
-          loaded: true,
-          resp: resp
-        })
-      });
-  }
-
-  componentWillMount() {
-    let page = this.props.params.page || 1;
-
-    this.fetchPosts(page)
-  }
-
-  componentWillReceiveProps(nextProps) {
-    this.setState({loaded: false});
-
-    let page = nextProps.params.page || 1;
-
-    this.fetchPosts(page)
-  }
-
-  render() {
-    if (this.state.loaded) {
-      return (
-        <div>
-          <Helmet>
-            <title>Nat? Nat. Nat!</title>
-          </Helmet>
-        {this.state.resp.map((post) => {
-          return (
-            <div className="vat pa2 mv2 dib w-100 mh0">
-              <h2 className="lh-title fw1 ph0 mb1 mt2">
-                <Link className="no-underline" to={`/post/${post.id}`} key={post.id}>{post.title}</Link>
-              </h2>
-              <div>
-                <p className="mv0 f6">#{post.id} <Moment className="dib" interval={0} format="MMM. Do YYYY, h a z">{post.date}</Moment>. {Math.ceil(post.readtime / 60)} minute read.</p>
-              </div>
-              <div className="f5 db lh-copy">
-                <div className="dib" dangerouslySetInnerHTML={{__html: post.html}}></div>
-
-                <div><Link to={`/post/${post.id}`} key={post.id}>Continue Reading...</Link></div>
-              </div>
+function Home({ data: { Posts, refetch } }) {
+  return (
+      <div>
+        <Helmet>
+          <title>Nat? Nat. Nat!</title>
+        </Helmet>
+      {Posts && Posts.map((post) => {
+        return (
+          <div className="vat pa2 mv2 dib w-100 mh0">
+            <h2 className="lh-title fw1 ph0 mb1 mt2">
+              <Link className="no-underline" to={`/post/${post.Id}`} key={post.Id}>{post.Title}</Link>
+            </h2>
+            <div>
+              <p className="mv0 f6">#{post.Id} <Moment className="dib" interval={0} format="MMM. Do YYYY, h a z">{post.Datetime}</Moment>. {Math.ceil(post.Readtime / 60)} minute read.</p>
             </div>
-          )
-        })}
-        </div>
-      );
-    } else {
-      return (
-        <div>
-        Loading...
-        </div>
-      )
-    }
-  }
+            <div className="f5 db lh-copy">
+              <div className="dib" dangerouslySetInnerHTML={{__html: post.SummaryHtml}}></div>
+
+              <div><Link to={`/post/${post.Id}`} key={post.Id}>Continue Reading...</Link></div>
+            </div>
+          </div>
+        )
+      })}
+      </div>
+  );
 }
 
+// TODO: add Readtime, SummaryHtml
 export default graphql(gql`
   query HomeQuery {
-    posts {
-      id
-      title
-      date
+    Posts {
+      Id
+      Title
+      Datetime
     }
   }
 `)(Home);
