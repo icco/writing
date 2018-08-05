3789959b627b2fd3edada61a21430fab2f18ba37
Nat Welch <nat@natwelch.com>
2018-08-05 17:56:52 +0000

    things that didn't work

--- next.config.js
+++ next.config.js
@@ -5,3 +5,10 @@ const withMDX = require('@zeit/next-mdx')({
 module.exports = withMDX({
   pageExtensions: ['js', 'jsx', 'mdx']
 })
+
+
+// https://github.com/zeit/next-plugins/tree/master/packages/next-css
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS({
+  cssModules: true
+})
