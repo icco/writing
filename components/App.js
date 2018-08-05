286df88858bbcb9f2ea3c273344065593a0aebfa
Nat Welch <nat@natwelch.com>
2018-08-05 05:29:40 +0000

    first pass

--- components/App.js
+++ components/App.js
@@ -18,25 +18,6 @@ export default ({ children }) => (
         font-size: 14px;
         line-height: 24px;
       }
-      article {
-        margin: 0 auto;
-        max-width: 650px;
-      }
-      button {
-        align-items: center;
-        background-color: #22bad9;
-        border: 0;
-        color: white;
-        display: flex;
-        padding: 5px 7px;
-      }
-      button:active {
-        background-color: #1b9db7;
-        transition: background-color 0.3s;
-      }
-      button:focus {
-        outline: none;
-      }
     `}</style>
   </main>
 )
--- components/PostList.js
+++ components/PostList.js
@@ -1,7 +1,6 @@
 import { graphql } from 'react-apollo'
 import gql from 'graphql-tag'
 import ErrorMessage from './ErrorMessage'
-import PostUpvoter from './PostUpvoter'
 
 const POSTS_PER_PAGE = 10
 
@@ -11,65 +10,19 @@ function PostList ({
 }) {
   if (error) return <ErrorMessage message='Error loading posts.' />
   if (allPosts && allPosts.length) {
-    const areMorePosts = allPosts.length < _allPostsMeta.count
     return (
       <section>
         <ul>
-          {allPosts.map((post, index) => (
+          {allPosts.map((post) => (
             <li key={post.id}>
               <div>
-                <span>{index + 1}. </span>
-                <a href={post.url}>{post.title}</a>
-                <PostUpvoter id={post.id} votes={post.votes} />
+                <span>#{post.id} </span>
+                <a href={`/post/${post.id}`}>{post.title}</a>
               </div>
             </li>
           ))}
         </ul>
-        {areMorePosts ? (
-          <button onClick={() => loadMorePosts()}>
-            {' '}
-            {loading ? 'Loading...' : 'Show More'}{' '}
-          </button>
-        ) : (
-          ''
-        )}
         <style jsx>{`
-          section {
-            padding-bottom: 20px;
-          }
-          li {
-            display: block;
-            margin-bottom: 10px;
-          }
-          div {
-            align-items: center;
-            display: flex;
-          }
-          a {
-            font-size: 14px;
-            margin-right: 10px;
-            text-decoration: none;
-            padding-bottom: 0;
-            border: 0;
-          }
-          span {
-            font-size: 14px;
-            margin-right: 5px;
-          }
-          ul {
-            margin: 0;
-            padding: 0;
-          }
-          button:before {
-            align-self: center;
-            border-style: solid;
-            border-width: 6px 4px 0 4px;
-            border-color: #ffffff transparent transparent transparent;
-            content: '';
-            height: 0;
-            margin-right: 5px;
-            width: 0;
-          }
         `}</style>
       </section>
     )
@@ -78,29 +31,18 @@ function PostList ({
 }
 
 export const allPosts = gql`
-  query allPosts($first: Int!, $skip: Int!) {
-    allPosts(orderBy: createdAt_DESC, first: $first, skip: $skip) {
+  query allPosts {
+    allPosts {
       id
       title
-      votes
-      url
-      createdAt
-    }
-    _allPostsMeta {
-      count
     }
   }
 `
-export const allPostsQueryVars = {
-  skip: 0,
-  first: POSTS_PER_PAGE
-}
 
 // The `graphql` wrapper executes a GraphQL query and makes the results
 // available on the `data` prop of the wrapped component (PostList)
 export default graphql(allPosts, {
   options: {
-    variables: allPostsQueryVars
   },
   props: ({ data }) => {
     return ({
