dfde9608dbba38109d11d8f61acd93a106ed1858
Nat Welch <nat@natwelch.com>
2018-08-06 03:36:26 +0000

    working css

---
+++ pages/_document.js
@@ -0,0 +1,29 @@
+// _document is only rendered on the server side and not on the client side
+// Event handlers like onClick can't be added to this file
+
+// ./pages/_document.js
+import Document, { Head, Main, NextScript } from 'next/document'
+
+// CSS is compiled into the style.css below
+import "../css/tachyons.min.css"
+
+export default class WritingDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html lang="en">
+        <Head>
+          <link rel="stylesheet" href="/_next/static/style.css" />
+        </Head>
+        <body className="ma2">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}
