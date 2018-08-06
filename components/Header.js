498f1aaa4cdbf4156ba9e130823f133c315daddd
Nat Welch <nat@natwelch.com>
2018-08-06 04:43:41 +0000

    doesn't quite work

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,10 @@
 import Link from "next/link";
 import { withRouter } from "next/router";
+import Logo from "./Logo";
 
 const Header = ({ router: { pathname } }) => (
   <header className="mb3">
+    <Logo />
     <Link prefetch href="/">
       <a className={pathname === "/" ? "is-active" : ""}>Home</a>
     </Link>
--- components/Logo.js
+++ components/Logo.js
@@ -1,57 +1,61 @@
-import P5Wrapper from 'react-p5-wrapper';
+import dynamic from 'next/dynamic';
+
+const P5Wrapper = dynamic(import('react-p5-wrapper'), {
+  loading: () => <p>Loading...</p>,
+  ssr: false,
+});
 
 // spinning logo
-  function sketch (p) {
-    let t = 0
-    let rand = []
+function sketch (p) {
+  let t = 0
+  let rand = []
 
-    p.setup = function () {
-      var cnv = createCanvas(200, 200);
-      cnv.parent('logo');
+  p.setup = function () {
+    var cnv = p.createCanvas(200, 200);
+    cnv.parent('.logo');
 
-      noStroke();
-      fill(51);
-      rand = [
-        random(-180, 180),
-        random(-180, 180),
-        random(-180, 180),
-        random(-180, 180)
-      ]
+    p.noStroke();
+    p.fill(51);
+    rand = [
+      p.random(-180, 180),
+      p.random(-180, 180),
+      p.random(-180, 180),
+      p.random(-180, 180)
+    ]
+  }
+
+  p.draw = function () {
+    if (!(round(t) % 12)) {
+      p.background(256, 60);
     }
 
-    p.draw = function () {
-      if (!(round(t) % 12)) {
-        background(256, 60);
-      }
+    var k = (p.width / 4);
 
-      var p = (width / 4);
+    [
+      [ k * 1, k * 1 ],
+      [ k * 3, k * 1 ],
+      [ k * 1, k * 3 ],
+      [ k * 3, k * 3 ]
+    ].forEach(function(arr, i) {
+      let x = arr[0]
+      let y = arr[1]
+      let r = rand[i]
 
-      [
-        [ p * 1, p * 1 ],
-        [ p * 3, p * 1 ],
-        [ p * 1, p * 3 ],
-        [ p * 3, p * 3 ]
-      ].forEach(function(arr, i) {
-        let x = arr[0]
-        let y = arr[1]
-        let r = rand[i]
+      // each particle moves in a circle
+      let myX = x + 30 * cos(2 * p.PI * t + r);
+      let myY = y + 30 * sin(2 * p.PI * t + r);
 
-        // each particle moves in a circle
-        let myX = x + 30 * cos(2 * PI * t + r);
-        let myY = y + 30 * sin(2 * PI * t + r);
-
-        ellipse(myX, myY, 8); // draw particle
-      })
+      p.ellipse(myX, myY, 8); // draw particle
+    })
 
 
-      t = t + 0.01; // update time
-    }
+    t = t + 0.01; // update time
+  }
+}
 
 function round(x) {
   return Number.parseFloat(x).toFixed(4);
 }
-
-
 
 const Link = (params) => (
   <P5Wrapper sketch={sketch} />
