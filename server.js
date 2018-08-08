150e9a1ea714cb3262da25acd5b5f70f697d96c1
Nat Welch <nat@natwelch.com>
2018-08-08 02:37:57 +0000

    progress

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require("express");
 const next = require("next");
+const rss = require("rss");
+const gql = require("graphql-tag");
+const apollo = require("./lib/apollo.js");
 
 const dev = process.env.NODE_ENV !== "production";
 const app = next({ dev });
@@ -16,11 +19,32 @@ app
       app.render(req, res, actualPage, queryParams);
     });
 
+    server.get("/feed.rss", (req, res) => {
+      const client = apollo.create();
+      client
+        .query({
+          query: gql`
+            query allPosts {
+              allPosts {
+                id
+                title
+                datetime
+              }
+            }
+          `
+        })
+        .then(data => {
+         console.log( data.data.allPosts)
+        });
+
+      res.send("hello");
+    });
+
     server.get("*", (req, res) => {
       return handle(req, res);
     });
 
-    server.listen(8080, '0.0.0.0', err => {
+    server.listen(8080, "0.0.0.0", err => {
       if (err) throw err;
       console.log("> Ready on http://localhost:8080");
     });
