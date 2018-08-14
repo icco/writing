36590fe16d6e9032623bafc8938e06b4d27c1c7a
Nat Welch <nat@natwelch.com>
2018-08-14 12:49:53 +0000

    delete stats for now

--- components/Footer.js
+++ components/Footer.js
@@ -34,33 +34,6 @@ const Header = params => (
         <Twitter />
       </a>
     </div>
-
-    <div className="cf">
-      <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-        <dd className="f6 fw4 ml0">Closed Issues</dd>
-        <dd className="f3 fw6 ml0">1,024</dd>
-      </dl>
-      <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-        <dd className="f6 fw4 ml0">Open Issues</dd>
-        <dd className="f3 fw6 ml0">993</dd>
-      </dl>
-      <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-        <dd className="f6 fw4 ml0">Next Release</dd>
-        <dd className="f3 fw6 ml0">10-22</dd>
-      </dl>
-      <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-        <dd className="f6 fw4 ml0">Days Left</dd>
-        <dd className="f3 fw6 ml0">4</dd>
-      </dl>
-      <dl className="fl fn-l w-50 dib-l w-auto-l lh-title mr5-l">
-        <dd className="f6 fw4 ml0">Favorite Cat</dd>
-        <dd className="f3 fw6 ml0">All of Them</dd>
-      </dl>
-      <dl className="fl fn-l w-50 dib-l w-auto-l lh-title">
-        <dd className="f6 fw4 ml0">App Downloads</dd>
-        <dd className="f3 fw6 ml0">1,200</dd>
-      </dl>
-    </div>
   </footer>
 );
 
