226ec7b1abc1b2810413d28db3d75e394a46d7c0
Nat Welch <nat@natwelch.com>
2018-08-19 19:15:52 +0000

    sure

--- components/Auth.js
+++ components/Auth.js
@@ -1,6 +1,10 @@
-import history from '../history';
 import auth0 from 'auth0-js';
-import { AUTH_CONFIG } from './auth0-variables';
+
+const AUTH_CONFIG = {
+  domain: '{DOMAIN}',
+  clientId: '{CLIENT_ID}',
+  callbackUrl: 'http://localhost:3000/callback'
+}
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
@@ -27,9 +31,7 @@ export default class Auth {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
-        history.replace('/home');
       } else if (err) {
-        history.replace('/home');
         console.log(err);
         alert(`Error: ${err.error}. Check the console for further details.`);
       }
@@ -43,7 +45,6 @@ export default class Auth {
     localStorage.setItem('id_token', authResult.idToken);
     localStorage.setItem('expires_at', expiresAt);
     // navigate to the home route
-    history.replace('/home');
   }
 
   logout() {
@@ -52,7 +53,6 @@ export default class Auth {
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
     // navigate to the home route
-    history.replace('/home');
   }
 
   isAuthenticated() {
