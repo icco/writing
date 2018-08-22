26bef986bd629be93334115ea4ab0913590f44da
Nat Welch <nat@natwelch.com>
2018-08-22 21:35:29 +0000

    First pass at regex

--- components/Post.js
+++ components/Post.js
@@ -22,6 +22,56 @@ let md = new MarkdownIt({
   linkify: true
 }).use(require("markdown-it-footnote"));
 
+// Add Twitter User support
+md.linkify.add('@', {
+  validate: function (text, pos, self) {
+    var tail = text.slice(pos);
+
+    if (!self.re.twitter) {
+      self.re.twitter =  new RegExp(
+        '^([a-zA-Z0-9_]){1,15}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+      );
+    }
+    if (self.re.twitter.test(tail)) {
+      // Linkifier allows punctuation chars before prefix,
+      // but we additionally disable `@` ("@@mention" is invalid)
+      if (pos >= 2 && tail[pos - 2] === '@') {
+        return false;
+      }
+      return tail.match(self.re.twitter)[0].length;
+    }
+    return 0;
+  },
+  normalize: function (match) {
+    match.url = 'https://twitter.com/' + match.url.replace(/^@/, '');
+  }
+});
+
+// Add hashtag support
+md.linkify.add('@', {
+  validate: function (text, pos, self) {
+    var tail = text.slice(pos);
+
+    if (!self.re.twitter) {
+      self.re.twitter =  new RegExp(
+        '^([a-zA-Z0-9_]){1,15}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+      );
+    }
+    if (self.re.twitter.test(tail)) {
+      // Linkifier allows punctuation chars before prefix,
+      // but we additionally disable `#` ("##tag" is invalid)
+      if (pos >= 2 && tail[pos - 2] === '@') {
+        return false;
+      }
+      return tail.match(self.re.twitter)[0].length;
+    }
+    return 0;
+  },
+  normalize: function (match) {
+    match.url = '/tag/' + match.url.replace(/^#/, '');
+  }
+});
+
 const Post = props => {
   const {
     id,
