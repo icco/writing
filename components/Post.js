a056b7931d7948271592af726cc149839d350cf1
Nat Welch <nat@natwelch.com>
2018-08-22 21:36:29 +0000

    Lint it all

--- components/Post.js
+++ components/Post.js
@@ -23,52 +23,52 @@ let md = new MarkdownIt({
 }).use(require("markdown-it-footnote"));
 
 // Add Twitter User support
-md.linkify.add('@', {
-  validate: function (text, pos, self) {
+md.linkify.add("@", {
+  validate: function(text, pos, self) {
     var tail = text.slice(pos);
 
     if (!self.re.twitter) {
-      self.re.twitter =  new RegExp(
-        '^([a-zA-Z0-9_]){1,15}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+      self.re.twitter = new RegExp(
+        "^([a-zA-Z0-9_]){1,15}(?!_)(?=$|" + self.re.src_ZPCc + ")"
       );
     }
     if (self.re.twitter.test(tail)) {
       // Linkifier allows punctuation chars before prefix,
       // but we additionally disable `@` ("@@mention" is invalid)
-      if (pos >= 2 && tail[pos - 2] === '@') {
+      if (pos >= 2 && tail[pos - 2] === "@") {
         return false;
       }
       return tail.match(self.re.twitter)[0].length;
     }
     return 0;
   },
-  normalize: function (match) {
-    match.url = 'https://twitter.com/' + match.url.replace(/^@/, '');
+  normalize: function(match) {
+    match.url = "https://twitter.com/" + match.url.replace(/^@/, "");
   }
 });
 
 // Add hashtag support
-md.linkify.add('@', {
-  validate: function (text, pos, self) {
+md.linkify.add("@", {
+  validate: function(text, pos, self) {
     var tail = text.slice(pos);
 
     if (!self.re.twitter) {
-      self.re.twitter =  new RegExp(
-        '^([a-zA-Z0-9_]){1,15}(?!_)(?=$|' + self.re.src_ZPCc + ')'
+      self.re.twitter = new RegExp(
+        "^([a-zA-Z0-9_]){1,15}(?!_)(?=$|" + self.re.src_ZPCc + ")"
       );
     }
     if (self.re.twitter.test(tail)) {
       // Linkifier allows punctuation chars before prefix,
       // but we additionally disable `#` ("##tag" is invalid)
-      if (pos >= 2 && tail[pos - 2] === '@') {
+      if (pos >= 2 && tail[pos - 2] === "@") {
         return false;
       }
       return tail.match(self.re.twitter)[0].length;
     }
     return 0;
   },
-  normalize: function (match) {
-    match.url = '/tag/' + match.url.replace(/^#/, '');
+  normalize: function(match) {
+    match.url = "/tag/" + match.url.replace(/^#/, "");
   }
 });
 
