6ef8c7d37a9b51159703ae9f77a69d711209fdca
Nat Welch <nat@natwelch.com>
2018-09-01 23:40:54 +0000

    this doesn't work...

--- components/Footer.js
+++ components/Footer.js
@@ -4,77 +4,89 @@ import Github from "../svgs/github.svg";
 import ErrorMessage from "./ErrorMessage";
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
+import { NextAuth } from 'next-auth/client'
+import React from 'react'
 
 // SVG Icons are from https://simpleicons.org
-const Footer = params => {
-  if (params.data.error) return <ErrorMessage message="Error loading stats." />;
-  let stats = "";
-  if (params.data.stats && params.data.stats.length) {
-    stats = (
-      <div className="cf">
-        {params.data.stats.map(({ key, value }) => (
-          <dl key={key} className="fl fn-l w-50 dib-l w-auto-l lh-title">
-            <dd className="f6 fw4 ml0">{key}</dd>
-            <dd className="f3 fw6 ml0">{value}</dd>
-          </dl>
-        ))}
-      </div>
-    );
+class Footer extends React.Component {
+  static async getInitialProps(params) {
+    return {
+      session: NextAuth.init({req: params.req}),
+      data: params.data
+    }
   }
 
-  return (
-    <footer className="lh-title mv5 pv5 pl4 pr3 ph5-ns bt b--black-10">
-      <h3 className="f6 tracked">
-        Nat? Nat. Nat! is the blog of{" "}
-        <a className="link" href="https://natwelch.com">
-          Nat Welch
-        </a>
-        .
-      </h3>
+  render() {
+    console.log(this.props.session)
+    if (this.props.data.error) return <ErrorMessage message="Error loading stats." />;
+    let stats = "";
+    if (this.props.data.stats && this.props.data.stats.length) {
+      stats = (
+        <div className="cf">
+          {this.props.data.stats.map(({ key, value }) => (
+            <dl key={key} className="fl fn-l w-50 dib-l w-auto-l lh-title">
+              <dd className="f6 fw4 ml0">{key}</dd>
+              <dd className="f3 fw6 ml0">{value}</dd>
+            </dl>
+          ))}
+        </div>
+      );
+    }
 
-      <div className="">
-        <a
-          className="link near-black hover-silver dib h1 w1 mr3"
-          href="https://github.com/icco"
-          title="Nat Welch GitHub"
-        >
-          <Github />
-        </a>
-        <a
-          className="link hover-silver near-black dib h1 w1 mr3"
-          href="https://instagram.com/probablynatwelch"
-          title="Nat Welch Instagram"
-        >
-          <Instagram />
-        </a>
-        <a
-          className="link hover-silver near-black dib h1 w1 mr3"
-          href="https://twitter.com/icco"
-          title="Nat Welch Twitter"
-        >
-          <Twitter />
-        </a>
-      </div>
+    return (
+      <footer className="lh-title mv5 pv5 pl4 pr3 ph5-ns bt b--black-10">
+        <h3 className="f6 tracked">
+          Nat? Nat. Nat! is the blog of{" "}
+          <a className="link" href="https://natwelch.com">
+            Nat Welch
+          </a>
+          .
+        </h3>
 
-      {stats}
+        <div className="">
+          <a
+            className="link near-black hover-silver dib h1 w1 mr3"
+            href="https://github.com/icco"
+            title="Nat Welch GitHub"
+          >
+            <Github />
+          </a>
+          <a
+            className="link hover-silver near-black dib h1 w1 mr3"
+            href="https://instagram.com/probablynatwelch"
+            title="Nat Welch Instagram"
+          >
+            <Instagram />
+          </a>
+          <a
+            className="link hover-silver near-black dib h1 w1 mr3"
+            href="https://twitter.com/icco"
+            title="Nat Welch Twitter"
+          >
+            <Twitter />
+          </a>
+        </div>
 
-      <div className="mv2">
-        Try out the{" "}
-        <a className="link" href="https://brave.com/nat432">
-          Brave Browser
-        </a>
-        .
-      </div>
+        {stats}
 
-      <div className="mv2 rc-scout" />
+        <div className="mv2">
+          Try out the{" "}
+          <a className="link" href="https://brave.com/nat432">
+            Brave Browser
+          </a>
+          .
+        </div>
 
-      <script
-        async
-        defer
-        src="https://www.recurse-scout.com/loader.js?t=1a20cf01214e4c5923ab6ebd6c0f8f18"
-      />
-    </footer>
-  );
+        <div className="mv2 rc-scout" />
+
+        <script
+          async
+          defer
+          src="https://www.recurse-scout.com/loader.js?t=1a20cf01214e4c5923ab6ebd6c0f8f18"
+        />
+      </footer>
+    );
+  }
 };
 
 export const allStats = gql`
