547296c1656ace8c1fe7932badbe592b2a711d5c
Nat Welch <nat@natwelch.com>
2018-09-02 01:12:17 +0000

    blank admin page

---
+++ pages/admin.js
@@ -0,0 +1,16 @@
+import App from "../components/App";
+import Header from "../components/Header";
+import Head from "next/head";
+
+export default (props) => {
+  console.log(props)
+  
+  
+  return(
+  <App>
+    <Head>
+      <title>Nat? Nat. Nat! | Admin</title>
+    </Head>
+    <Header />
+  </App>
+)};
