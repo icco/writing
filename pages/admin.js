4978bcd09a44c890fff8c88ddfd4f8f1fe38490e
Nat Welch <nat@natwelch.com>
2018-09-02 01:44:53 +0000

    admin page, no session

--- pages/admin.js
+++ pages/admin.js
@@ -1,16 +1,30 @@
 import App from "../components/App";
 import Header from "../components/Header";
+import Footer from "../components/Footer";
 import Head from "next/head";
+import { Component } from "react";
+import { NextAuth } from "next-auth/client";
 
-export default props => {
-  console.log(props);
+export default class extends Component {
+  static async getInitialProps(ctx) {
+    const sessData = await NextAuth.init({ req: ctx.req });
+    return {
+      session: sessData
+    };
+  }
 
-  return (
-    <App>
-      <Head>
-        <title>Nat? Nat. Nat! | Admin</title>
-      </Head>
-      <Header />
-    </App>
-  );
-};
+  render(props) {
+    console.log(props);
+
+    return (
+      <App>
+        <Head>
+          <title>Nat? Nat. Nat! | Admin</title>
+        </Head>
+        <Header />
+
+        <Footer />
+      </App>
+    );
+  }
+}
