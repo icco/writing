ab086bf7c0bf6dbc711b93e8f7c7ea16a096049f
Nat Welch <nat@natwelch.com>
2018-09-14 22:53:11 +0000

    Fix formatting

--- components/Datetime.js
+++ components/Datetime.js
@@ -1,10 +1,10 @@
 import { DateTime } from "luxon";
 
 const Datetime = props => {
-  let time = DateTime.fromISO(props.children);
+  let time = DateTime.fromISO(props.children).toUTC();
 
   return (
-    <time title={time.toISO()} datetime={time.toISO()}>
+    <time title={time.toRFC2822()} dateTime={time.toISO()}>
       {time.toISODate()}
     </time>
   );
