b63b908a9a561a5bd6dba6b615916a80c4d93be5
Nat Welch <nat@natwelch.com>
2018-10-07 14:25:57 +0000

    lint

--- components/App.js
+++ components/App.js
@@ -1,9 +1,9 @@
-import React from 'react'
-import Router from 'next/router'
+import React from "react";
+import Router from "next/router";
 import * as gtag from "../lib/gtag";
 
 Router.onRouteChangeComplete = url => {
-  gtag.pageview(url)
-}
+  gtag.pageview(url);
+};
 
 export default ({ children }) => <main>{children}</main>;
