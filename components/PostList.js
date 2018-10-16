d782d35ccd539f7edb43d932c0eb166be6740e60
Nat Welch <nat@natwelch.com>
2018-10-16 21:07:38 +0000

    Won't work, but first pass

--- components/PostList.js
+++ components/PostList.js
@@ -3,27 +3,30 @@ import gql from "graphql-tag";
 import ErrorMessage from "./ErrorMessage";
 import Link from "next/link";
 import Datetime from "./Datetime";
+import InfiniteScroll from "react-infinite-scroller";
 
-function PostList({ data: { loading, error, allPosts } }) {
+function PostList({ data: { loading, error, posts, loadMore } }) {
   if (error) return <ErrorMessage message="Error loading posts." />;
-  if (allPosts && allPosts.length) {
+  if (posts && posts.length) {
     return (
       <section className="mw8 center">
-        <ul className="list pl0">
-          {allPosts.map(post => (
-            <li className="mb5 ml4 mr3" key={post.id}>
-              <div className="f6 db pb1 gray">
-                <span className="mr3">#{post.id}</span>
-                <Datetime>{post.datetime}</Datetime>
-              </div>
-              <Link as={`/post/${post.id}`} href={`/post?id=${post.id}`}>
-                <a className="header db f3 f1-ns link dark-gray dim">
-                  {post.title}
-                </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
+        <InfiniteScroll loadMore={loadMore} loader={<p>Loading...</p>}>
+          <ul className="list pl0">
+            {allPosts.map(post => (
+              <li className="mb5 ml4 mr3" key={post.id}>
+                <div className="f6 db pb1 gray">
+                  <span className="mr3">#{post.id}</span>
+                  <Datetime>{post.datetime}</Datetime>
+                </div>
+                <Link as={`/post/${post.id}`} href={`/post?id=${post.id}`}>
+                  <a className="header db f3 f1-ns link dark-gray dim">
+                    {post.title}
+                  </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </InfiniteScroll>
       </section>
     );
   }
@@ -32,8 +35,8 @@ function PostList({ data: { loading, error, allPosts } }) {
 }
 
 export const allPosts = gql`
-  query allPosts {
-    allPosts {
+  query posts($offset: Int!) {
+    posts(limit: 10, offset: $offset {
       id
       title
       datetime
@@ -44,5 +47,28 @@ export const allPosts = gql`
 // The `graphql` wrapper executes a GraphQL query and makes the results
 // available on the `data` prop of the wrapped component (PostList)
 export default graphql(allPosts, {
+  variables: {
+    offset: 0,
+  },
   options: {},
 })(PostList);
+
+const withQuery = graphql(QUERY_REPOS, {
+  props: ({ data }) => ({
+    data: {
+      ...data,
+      loadMore: () =>
+        data.fetchMore({
+          variables: { offset: 10 },
+          updateQuery: (previousResult = {}, { fetchMoreResult = {} }) => {
+            console.log(previousResult, fetchMoreResult);
+            return {
+              data: {
+                posts: [...previousPosts, ...currentPosts],
+              },
+            };
+          },
+        }),
+    },
+  }),
+});
