d6b194a16570c50baacc3db4ef5f9b03b74eee1c
Nat Welch <nat@natwelch.com>
2018-10-17 16:48:27 +0000

    Add loading

--- components/Loading.js
+++ components/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactLoading from "react-loading";
 
 const Loading = () => (
-  <ReactLoading type={"bars"} color={"#333"} height={100} width={100} />
+  <ReactLoading type={"bars"} height={64} width={64} className="center grey" />
 );
 
 export default Loading;
