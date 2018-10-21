b0c4175abc45afd8fb6d97466ce20016c9d8d693
Nat Welch <nat@natwelch.com>
2018-10-21 02:10:42 +0000

    first tracking code

--- pages/_document.js
+++ pages/_document.js
@@ -32,6 +32,23 @@ export default class WritingDocument extends Document {
           `,
             }}
           />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+(function(f, a, t, h, o, m){
+	a[h]=a[h]||function(){
+		(a[h].q=a[h].q||[]).push(arguments)
+	};
+	o=f.createElement('script'),
+	m=f.getElementsByTagName('script')[0];
+	o.async=1; o.src=t; o.id='fathom-script';
+	m.parentNode.insertBefore(o,m)
+})(document, window, '//a.natwelch.com/tracker.js', 'fathom');
+fathom('set', 'siteId', 'WGVFU');
+fathom('trackPageview');
+          `,
+            }}
+          />
         </Head>
         <body>
           <Main />
