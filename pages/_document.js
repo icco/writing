6d1a6332bab78310df32c9f35a58e64f891ce850
Nat Welch <nat@natwelch.com>
2018-10-21 17:00:25 +0000

    new fathom

--- pages/_document.js
+++ pages/_document.js
@@ -35,17 +35,16 @@ export default class WritingDocument extends Document {
           <script
             dangerouslySetInnerHTML={{
               __html: `
-(function(f, a, t, h, o, m){
-	a[h]=a[h]||function(){
-		(a[h].q=a[h].q||[]).push(arguments)
-	};
-	o=f.createElement('script'),
-	m=f.getElementsByTagName('script')[0];
-	o.async=1; o.src=t; o.id='fathom-script';
-	m.parentNode.insertBefore(o,m)
-})(document, window, '//a.natwelch.com/tracker.js', 'fathom');
-fathom('set', 'siteId', 'WGVFU');
-fathom('trackPageview');
+            (function(f, a, t, h, o, m){
+              a[h]=a[h]||function(){
+                (a[h].q=a[h].q||[]).push(arguments)
+              };
+              o=f.createElement('script'),
+              m=f.getElementsByTagName('script')[0];
+              o.async=1; o.src=t; o.id='fathom-script';
+              m.parentNode.insertBefore(o,m)
+            })(document, window, '//a.natwelch.com/tracker.js', 'fathom');
+            fathom('set', 'siteId', '${TRACKING_ID}');
           `,
             }}
           />
