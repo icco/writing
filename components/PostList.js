0bb4e3de35e98642bfe9a22a03cdf384f57ebf6f
Nat Welch <nat@natwelch.com>
2018-10-30 23:26:54 +0000

    Add tags to post meta

--- components/PostList.js
+++ components/PostList.js
@@ -25,6 +25,13 @@ function PostList({ data: { error, posts, loadMore } }) {
                 <div className="f6 db pb1 gray">
                   <span className="dbi mr3">#{post.id}</span>
                   <Datetime>{post.datetime}</Datetime>
+                  <span className="dbi ml3">
+                    {post.tags.map((tag, i) => (
+                      <Link key={i} as={`/tag/${tag}`} href={`/tag?id=${tag}`}>
+                        <a className="mh1 link gray dim">#{tag}</a>
+                      </Link>
+                    ))}
+                  </span>
                 </div>
                 <Link as={`/post/${post.id}`} href={`/post?id=${post.id}`}>
                   <a className="header db f3 f1-ns link dark-gray dim">
@@ -48,6 +55,7 @@ export const allPosts = gql`
       id
       title
       datetime
+      tags
     }
   }
 `;
