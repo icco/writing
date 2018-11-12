afaff2be7d2e5f69adb13ec314d1a8fbdbf599b6
Nat Welch <nat@natwelch.com>
2018-11-12 02:36:18 +0000

    fix css

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 // https://github.com/zeit/next-plugins/tree/master/packages/next-css
 const withCSS = require("@zeit/next-css");
-module.exports = {
-  withCSS: withCSS(),
+module.exports = withCSS({
   serverRuntimeConfig: {
     // Will only be available on the server side
   },
@@ -9,4 +8,4 @@ module.exports = {
     // Will be available on both server and client
     graphql_origin: "https://graphql.natwelch.com",
   },
-};
+});
