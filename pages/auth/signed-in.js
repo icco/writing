1f329dd61974d030f2e0773d318a61eadfc2a852
Nat Welch <nat@natwelch.com>
2019-01-20 03:22:25 +0000

    more auth cleanup

---
+++ pages/auth/signed-in.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Router from 'next/router'
+
+import { setToken } from '../../lib/auth'
+import { parseHash } from '../../lib/auth0'
+
+export default class SignedIn extends React.Component {
+  componentDidMount () {
+    parseHash((err, result) => {
+      if(err) {
+        console.error('Something happened with the Sign In request')
+        return;
+      }
+
+      setToken(result.idToken, result.accessToken);
+      Router.push('/')
+    })
+  }
+
+  render () {
+    return null
+  }
+}
