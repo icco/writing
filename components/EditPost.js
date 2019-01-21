f563d4a1a24422773f46154820f137eb49bd9897
Nat Welch <nat@natwelch.com>
2019-01-21 19:22:21 +0000

    init react-mde

---
+++ components/EditPost.js
@@ -0,0 +1,36 @@
+import * as React from "react";
+import ReactMde from "../src";
+import * as Showdown from "showdown";
+
+export class EditPost extends React.Component {
+  constructor (props) {
+    super(props);
+    this.state = {
+      value: "**Hello world!!!**"
+    };
+    this.converter = new Showdown.Converter({
+      tables: true,
+      simplifiedAutoLink: true,
+      strikethrough: true,
+      tasklists: true
+    });
+  }
+
+  handleValueChange = (value) => {
+    this.setState({ value });
+  };
+
+  render () {
+    return (
+      <div className="container">
+        <ReactMde
+          onChange={this.handleValueChange}
+          value={this.state.value}
+          generateMarkdownPreview={markdown =>
+            Promise.resolve(this.converter.makeHtml(markdown))
+          }
+        />
+      </div>
+    );
+  }
+}
