15ff1ee3c147d4e511a7a7bca9b1a9e80d63bbf1
Nat Welch <nat@natwelch.com>
2019-01-21 16:15:43 +0000

    init

---
+++ pages/admin/post.js
@@ -0,0 +1,35 @@
+import App from "../components/App";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import EditPost from "../components/EditPost";
+import { withRouter } from "next/router";
+
+import { checkLoggedIn } from "../lib/auth";
+import { initApollo } from "../lib/init-apollo";
+
+export default class Page extends React.Component {
+  async componentDidMount() {
+    const { loggedInUser } = await checkLoggedIn(initApollo());
+    this.setState({ loggedInUser });
+  }
+
+  render() {
+    if (
+      !this.state ||
+      !this.state.loggedInUser ||
+      !this.state.loggedInUser.role ||
+      this.state.loggedInUser.role !== "admin"
+    ) {
+      return <Error statusCode={403} />;
+    }
+
+    return (
+  <App>
+    <Header />
+    <EditPost id={props.router.query.id} />
+  </App>
+);
+}
+}
+
+export default withRouter(Page);
