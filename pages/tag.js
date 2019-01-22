92e2efaa4a9ceed1a013ab560aab207e989c560b
Nat Welch <nat@natwelch.com>
2019-01-22 00:35:20 +0000

    Fix headers

--- pages/tag.js
+++ pages/tag.js
@@ -4,12 +4,22 @@ import Footer from "../components/Footer";
 import Tag from "../components/Tag";
 import { withRouter } from "next/router";
 
+import { checkLoggedIn } from "../lib/auth";
+
 const Page = withRouter(props => (
   <App>
-    <Header />
+    <Header loggedInUser={props.loggedInUser} />
     <Tag id={props.router.query.id} />
     <Footer />
   </App>
 ));
+
+Page.getInitialProps = async ctx => {
+  const { loggedInUser } = await checkLoggedIn(ctx.apolloClient);
+
+  return {
+    loggedInUser,
+  };
+};
 
 export default Page;
