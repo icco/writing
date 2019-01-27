134e4ac17be7dc4184b2dcc6fbefca23439a8aa9
Nat Welch <nat@natwelch.com>
2019-01-27 23:21:53 +0000

    revert next config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 // https://github.com/zeit/next-plugins/tree/master/packages/next-css
 const withCSS = require("@zeit/next-css");
-module.exports = withCSS({});
+module.exports = withCSS({
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    graphql_origin: "https://graphql.natwelch.com",
+  },
+});
