724a1f15b4a1c1dc3113357e857da43f1f2a64bd
Nat Welch <nat@natwelch.com>
2019-03-17 17:07:39 +0000

    Inifinite Link List

--- components/AdminLinkList.js
+++ components/AdminLinkList.js
@@ -1,7 +1,11 @@
 import { graphql } from "react-apollo";
 import gql from "graphql-tag";
-import ErrorMessage from "./ErrorMessage";
 import Link from "next/link";
+import InfiniteScroll from "react-infinite-scroller";
+
+import ErrorMessage from "./ErrorMessage";
+import Datetime from "./Datetime";
+import Loading from "./Loading";
 
 function AdminLinkList({ data: { error, links } }) {
   if (error) {
@@ -11,14 +15,20 @@ function AdminLinkList({ data: { error, links } }) {
   if (links && links.length) {
     return (
       <section className="pa3 mw8 center">
+        <InfiniteScroll
+          threshold={500}
+          loadMore={loadMore}
+          hasMore={true}
+          loader={<Loading key="link-loader" />}
+        >
         <ul className="list pl0" key="link-ul">
           {links.map(l => (
             <li className="mv3" key={"link-" + l.id}>
-              <span className="mr3">{l.created}</span>
+              <span className="mr3"><Datetime>{l.created}</Datetime></span>
               <Link href={l.uri}>
                 <a className="link dark-blue dim">{l.title}</a>
-              </Link>{" "}
-              -{" "}
+              </Link>
+              {" - "}
               <span
                 className="gray link pointer dim"
                 onClick={() => {
@@ -32,6 +42,7 @@ function AdminLinkList({ data: { error, links } }) {
             </li>
           ))}
         </ul>
+        </InfiniteScroll>
       </section>
     );
   }
@@ -40,8 +51,8 @@ function AdminLinkList({ data: { error, links } }) {
 }
 
 export const SomeLinks = gql`
-  query someLinks {
-    links(input: { limit: 50, offset: 0 }) {
+  query links($offset: Int!, $perpage: Int!) {
+    links(input: { limit: $perpage, offset: $offset }) {
       id
       uri
       title
@@ -51,4 +62,31 @@ export const SomeLinks = gql`
   }
 `;
 
-export default graphql(SomeLinks, {})(AdminLinkList);
+const PER_PAGE = 50;
+
+export default graphql(SomeLinks, {
+  options: {
+    variables: {
+      offset: 0,
+      perpage: PER_PAGE,
+    },
+  },
+  props: ({ data }) => ({
+    data: {
+      ...data,
+      loadMore: page => {
+        var offset = page * PER_PAGE;
+        return data.fetchMore({
+          variables: { offset, perpage: PER_PAGE },
+          updateQuery: (previousResult = {}, { fetchMoreResult = {} }) => {
+            var previouslinks = previousResult.links;
+            var currentlinks = fetchMoreResult.links;
+            return {
+              links: [...previouslinks, ...currentlinks],
+            };
+          },
+        });
+      },
+    },
+  }),
+})(AdminLinkList);
