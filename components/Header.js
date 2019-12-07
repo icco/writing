17d74085bada535853bb415bd1ea2d58adfa7c2b
Nat Welch <nat@natwelch.com>
2019-12-07 03:26:47 +0000

    about

--- components/Header.js
+++ components/Header.js
@@ -7,6 +7,12 @@ class Header extends React.Component {
   render() {
     let prefix = <></>;
     let nav = (
+      <Link key="/about" href="/about">
+        <a className="f6 link dib dim mr3 black mr4-ns" href="/about">
+          about
+        </a>
+      </Link>
+
       <Link key="/auth/sign-in" href="/auth/sign-in">
         <a className="f6 link dib dim mr3 black mr4-ns" href="/auth/sign-in">
           sign in
