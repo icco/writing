9450577d6e471a2548ca8cc045f891420a027f2b
Nat Welch <nat@natwelch.com>
2019-12-27 21:09:55 +0000

    lint

--- components/PostList.js
+++ components/PostList.js
@@ -1,9 +1,9 @@
 import InfiniteScroll from "react-infinite-scroller";
 import Link from "next/link";
-import gql from 'graphql-tag'
+import gql from "graphql-tag";
 import { ErrorMessage, Loading } from "@icco/react-common";
-import { NetworkStatus } from 'apollo-client'
-import { useQuery } from '@apollo/react-hooks'
+import { NetworkStatus } from "apollo-client";
+import { useQuery } from "@apollo/react-hooks";
 
 import Datetime from "./Datetime";
 
@@ -21,9 +21,9 @@ export const allPosts = gql`
 `;
 
 export const allPostsQueryVars = {
-      offset: 0,
-      perpage: PER_PAGE,
-}
+  offset: 0,
+  perpage: PER_PAGE,
+};
 
 export default function PostList() {
   const { loading, error, data, fetchMore, networkStatus } = useQuery(
@@ -35,9 +35,9 @@ export default function PostList() {
       // more data
       notifyOnNetworkStatusChange: true,
     }
-  )
+  );
 
-  const loadingMorePosts = networkStatus === NetworkStatus.fetchMore
+  const loadingMorePosts = networkStatus === NetworkStatus.fetchMore;
 
   const loadMorePosts = () => {
     fetchMore({
@@ -46,52 +46,52 @@ export default function PostList() {
       },
       updateQuery: (previousResult, { fetchMoreResult }) => {
         if (!fetchMoreResult) {
-          return previousResult
+          return previousResult;
         }
         return Object.assign({}, previousResult, {
           // Append the new posts results to the old one
           posts: [...previousResult.posts, ...fetchMoreResult.posts],
-        })
+        });
       },
-    })
-  }
+    });
+  };
 
-  if (error) return <ErrorMessage message="Error loading posts." />
-  if (loading && !loadingMorePosts) return <Loading key={0} />
+  if (error) return <ErrorMessage message="Error loading posts." />;
+  if (loading && !loadingMorePosts) return <Loading key={0} />;
 
-  const { posts } = data
+  const { posts } = data;
 
-    return (
-      <section className="mw8 center">
-        <InfiniteScroll
-          threshold={500}
-          loadMore={loadMorePosts}
-          hasMore={true}
-          loader={<Loading key={0} />}
-        >
-          <ul className="list pl0" key="ul">
-            {posts.map(post => (
-              <li className="mb5 ml4 mr3" key={"post-list-" + post.id}>
-                <div className="f6 db pb1 gray">
-                  <span className="dbi mr3">#{post.id}</span>
-                  <Datetime>{post.datetime}</Datetime>
-                  <span className="dbi ml3">
-                    {post.tags.map((tag, i) => (
-                      <Link key={i} as={`/tag/${tag}`} href={`/tag/[id]`}>
-                        <a className="mh1 link gray dim">#{tag}</a>
-                      </Link>
-                    ))}
-                  </span>
-                </div>
-                <Link as={`/post/${post.id}`} href={`/post/[pid]`}>
-                  <a className="header db f3 f1-ns link dark-gray dim">
-                    {post.title}
-                  </a>
-                </Link>
-              </li>
-            ))}
-          </ul>
-        </InfiniteScroll>
-      </section>
-    );
-  }
+  return (
+    <section className="mw8 center">
+      <InfiniteScroll
+        threshold={500}
+        loadMore={loadMorePosts}
+        hasMore={true}
+        loader={<Loading key={0} />}
+      >
+        <ul className="list pl0" key="ul">
+          {posts.map(post => (
+            <li className="mb5 ml4 mr3" key={"post-list-" + post.id}>
+              <div className="f6 db pb1 gray">
+                <span className="dbi mr3">#{post.id}</span>
+                <Datetime>{post.datetime}</Datetime>
+                <span className="dbi ml3">
+                  {post.tags.map((tag, i) => (
+                    <Link key={i} as={`/tag/${tag}`} href={`/tag/[id]`}>
+                      <a className="mh1 link gray dim">#{tag}</a>
+                    </Link>
+                  ))}
+                </span>
+              </div>
+              <Link as={`/post/${post.id}`} href={`/post/[pid]`}>
+                <a className="header db f3 f1-ns link dark-gray dim">
+                  {post.title}
+                </a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </InfiniteScroll>
+    </section>
+  );
+}
