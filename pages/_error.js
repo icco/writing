4b05c505eae89a63d73cd2ebea49e0d86e356b45
Nat Welch <nat@natwelch.com>
2020-01-03 14:18:38 +0000

    lint

--- pages/_error.js
+++ pages/_error.js
@@ -5,8 +5,8 @@ function Error({ statusCode }) {
 }
 
 Error.getInitialProps = ({ res, err }) => {
-  const statusCode = res ? res.statusCode : err ? err.statusCode : 404
-  return { statusCode }
-}
+  const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
+  return { statusCode };
+};
 
-export default Error
+export default Error;
