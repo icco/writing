d32202b9ad757061df5a9dfe37dad845bc06d605
Nat Welch <nat@natwelch.com>
2020-01-13 13:26:09 +0000

    not quite there

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 import App from "../components/App";
 import Footer from "../components/Footer";
@@ -8,32 +9,47 @@ import { withApollo } from "../lib/apollo";
 
 const Page = props => {
   const router = useRouter();
+  const [value, setValue] = useState("");
   if (router == null) {
     return <></>;
   }
+
+  const { query } = router.query;
+  let results = <></>;
+
+  if (query && query != value) {
+    setValue(query);
+  }
+
+  if (value != "") {
+    results = <Search query={value} />;
+  }
+
   return (
     <App>
       <Header noLogo />
       <form className="">
-        <fieldset className="cf bn ma0 pa0">
-          <div className="cf">
+        <fieldset className="cf bn ma3 pa3">
+          <div class="cf">
             <input
-              className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
-              placeholder="Search"
               type="text"
-              name="search"
-              value=""
-              id="search"
+              class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-90 br2-ns br--left-ns"
+              placeholder="Search"
+              name="query"
+              value={value}
+              id="query"
+              onChange={e => setValue(e.target.value)}
             />
             <input
-              className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
               type="submit"
-              value="Search"
+              class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-10 br2-ns br--right-ns"
+              value="&#8618;"
             />
           </div>
         </fieldset>
       </form>
-      <Search query={router.query} />
+
+      {results}
       <Footer />
     </App>
   );
