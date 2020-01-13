0474ac12fc9330b135ac3472742933663ff51ed1
Nat Welch <nat@natwelch.com>
2020-01-13 02:03:48 +0000

    classname

--- pages/search.js
+++ pages/search.js
@@ -14,11 +14,11 @@ const Page = props => {
   return (
     <App>
       <Header noLogo />
-      <form class="">
-        <fieldset class="cf bn ma0 pa0">
-          <div class="cf">
+      <form className="">
+        <fieldset className="cf bn ma0 pa0">
+          <div className="cf">
             <input
-              class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
+              className="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
               placeholder="Search"
               type="text"
               name="search"
@@ -26,7 +26,7 @@ const Page = props => {
               id="search"
             />
             <input
-              class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
+              className="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
               type="submit"
               value="Search"
             />
