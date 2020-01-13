02bffbdb5cba2f3de401a99ebcb3e7a14419207a
Nat Welch <nat@natwelch.com>
2020-01-13 01:39:54 +0000

    lint

--- pages/search.js
+++ pages/search.js
@@ -17,8 +17,19 @@ const Page = props => {
       <form class="">
         <fieldset class="cf bn ma0 pa0">
           <div class="cf">
-            <input class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns" placeholder="Search" type="text" name="search" value="" id="search" />
-            <input class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns" type="submit" value="Search" />
+            <input
+              class="f6 f5-l input-reset bn fl black-80 bg-white pa3 lh-solid w-100 w-75-m w-80-l br2-ns br--left-ns"
+              placeholder="Search"
+              type="text"
+              name="search"
+              value=""
+              id="search"
+            />
+            <input
+              class="f6 f5-l button-reset fl pv3 tc bn bg-animate bg-black-70 hover-bg-black white pointer w-100 w-25-m w-20-l br2-ns br--right-ns"
+              type="submit"
+              value="Search"
+            />
           </div>
         </fieldset>
       </form>
