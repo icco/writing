4a527f3347373a8c46168d735f7549932b6f17e9
Nat Welch <nat@natwelch.com>
2020-08-01 18:29:27 +0000

    graphql-tag

--- components/PostList.js
+++ components/PostList.js
@@ -1,6 +1,6 @@
 import InfiniteScroll from "react-infinite-scroller";
 import Link from "next/link";
-import gql from "graphql-tag";
+import gql from "@apollo/client";
 import { ErrorMessage, Loading } from "@icco/react-common";
 import { NetworkStatus } from "@apollo/client";
 import { useQuery } from "@apollo/client";
--- pages/admin/new.js
+++ pages/admin/new.js
@@ -1,5 +1,5 @@
 import Router from "next/router";
-import gql from "graphql-tag";
+import gql from "@apollo/client";
 import { Mutation } from "react-apollo";
 import { Loading, ErrorMessage } from "@icco/react-common";
 import { withAuth, withLoginRequired } from "use-auth0-hooks";
