a0e351c1b4d8607d362d99c3b0d472c8f5c5eef2
Nat Welch <nat@natwelch.com>
2020-08-02 03:20:18 +0000

    lint

--- pages/post/[pid].js
+++ pages/post/[pid].js
@@ -1,11 +1,11 @@
 import { useRouter } from "next/router";
-import { useQuery, gql } from '@apollo/client'
+import { useQuery, gql } from "@apollo/client";
 
 import App from "../../components/App";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import Post, {getPost} from "../../components/Post";
-import { initializeApollo  } from "../../lib/apollo";
+import Post, { getPost } from "../../components/Post";
+import { initializeApollo } from "../../lib/apollo";
 
 const Page = (props) => {
   const router = useRouter();
@@ -25,37 +25,40 @@ const Page = (props) => {
 export async function getStaticProps(context) {
   const { pid } = context.params;
 
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo();
   await apolloClient.query({
     query: getPost,
-    variables: { "id": pid },
-  })
+    variables: { id: pid },
+  });
 
   return {
     props: {
       initialApolloState: apolloClient.cache.extract(),
     },
-  }
+  };
 }
 
 export async function getStaticPaths() {
-  const apolloClient = initializeApollo()
+  const apolloClient = initializeApollo();
 
   const result = await apolloClient.query({
-    query: gql`,
+    query: gql`
       query postIDs($offset: Int!, $perpage: Int!) {
         posts(input: { limit: $perpage, offset: $offset }) {
           id
         }
-      }`,
+      }
+    `,
     variables: {
       offset: 0,
       perpage: 500,
     },
-  })
+  });
 
   return {
-    paths: result["data"]["posts"].map(function(d) { return { "params": { "pid": d.id } }}),
+    paths: result["data"]["posts"].map(function (d) {
+      return { params: { pid: d.id } };
+    }),
     fallback: false,
   };
 }
