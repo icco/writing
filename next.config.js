a2a308d238e198cb5b50acb5d251598cd115b6bf
Nat Welch <nat@natwelch.com>
2020-08-02 02:28:57 +0000

    sure

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 const port = process.env.PORT || 8080;
 module.exports = {
+    trailingSlash: true,
   env: {
     GRAPHQL_ORIGIN: process.env.GRAPHQL_ORIGIN,
     AUTH0_CLIENT_ID: "MwFD0COlI4F4AWvOZThe1psOIletecnL",
