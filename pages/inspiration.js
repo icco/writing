0d17ecc0a034ec47198f016e18945d66ab994a75
Nat Welch <nat@natwelch.com>
2020-11-04 17:57:33 +0000

    sure

--- pages/inspiration.js
+++ pages/inspiration.js
@@ -1,7 +1,7 @@
-import Image from "next/image";
 import Head from "next/head";
 import fs from "fs";
 import { join } from "path";
+import Masonry from 'react-masonry-component';
 
 import App from "../components/App";
 import Header from "../components/Header";
@@ -15,11 +15,22 @@ const Inspiration = ({ images }) => {
       <Head>
         <title>Nat? Nat. Nat! Inspiration</title>
       </Head>
-      <Header />
-
+      <Header noLogo />
+    <Masonry
+                className={'cf pa2'}
+                options={{ transitionDuration: 0 }}
+                disableImagesLoaded={false} // default false
+                updateOnEachImageLoad={true} // default false and works only if disableImagesLoaded is false
+            >
       {images.map((i) => (
-        <Image src={`/imgs/${i}.png`} />
+        <div className="fl w-50 w-25-l pa2">
+        <img
+          src={`/imgs/inspiration/${i}`}
+          className="db w-100"
+        />
+    </div>
       ))}
+            </Masonry>
       <Footer />
     </App>
   );
@@ -28,7 +39,9 @@ const Inspiration = ({ images }) => {
 export async function getStaticProps() {
   let images = [];
   fs.readdirSync(imgdir).forEach((file) => {
-    images += file;
+    if (file.endsWith('.png')) {
+      images.push(file);
+    }
   });
 
   return {
