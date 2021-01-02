a5e61ffb88232effd48a29e9f0881a0a139dd62f
Nat Welch <nat@natwelch.com>
2021-01-02 22:08:44 +0000

    paths fails

--- pages/post/[pid].js
+++ pages/post/[pid].js
@@ -5,6 +5,7 @@ import App from "components/App";
 import Footer from "components/Footer";
 import Header from "components/Header";
 import Post, { getPost } from "components/Post";
+import { initializeApollo, addApolloState } from 'lib/apollo'
 
 const Page = (props) => {
   const router = useRouter();
@@ -23,48 +24,45 @@ const Page = (props) => {
   );
 };
 
-// export async function getStaticProps(context) {
-//   const { pid } = context.params;
-//
-//   const apolloClient = initializeApollo();
-//   await apolloClient.query({
-//     query: getPost,
-//     variables: { id: pid },
-//   });
-//
-//   return {
-//     props: {
-//       initialApolloState: apolloClient.cache.extract(),
-//       pid,
-//     },
-//     revalidate: 1,
-//   };
-// }
-//
-// export async function getStaticPaths() {
-//   const apolloClient = initializeApollo();
-//
-//   const result = await apolloClient.query({
-//     query: gql`
-//       query postIDs($offset: Int!, $perpage: Int!) {
-//         posts(input: { limit: $perpage, offset: $offset }) {
-//           id
-//         }
-//       }
-//     `,
-//     variables: {
-//       offset: 0,
-//       perpage: 2000,
-//     },
-//   });
-//
-//   return {
-//     paths: result["data"]["posts"].map(function (d) {
-//       return { params: { pid: d.id } };
-//     }),
-//     fallback: true,
-//     revalidate: 1,
-//   };
-// }
+export async function getStaticProps(context) {
+  const { pid } = context.params;
+
+  const apolloClient = initializeApollo();
+  await apolloClient.query({
+    query: getPost,
+    variables: { id: pid },
+  });
+
+  return addApolloState(apolloClient, {
+    props: { pid },
+    revalidate: 1,
+  })
+}
+
+export async function getStaticPaths() {
+  const apolloClient = initializeApollo();
+
+  const result = await apolloClient.query({
+    query: gql`
+      query postIDs($offset: Int!, $perpage: Int!) {
+        posts(input: { limit: $perpage, offset: $offset }) {
+          id
+        }
+      }
+    `,
+    variables: {
+      offset: 0,
+      perpage: 2000,
+    },
+  });
+
+  return {
+    paths: result["data"]["posts"].map(function (d) {
+      return { params: { pid: d.id } };
+    }),
+    fallback: true,
+    revalidate: 1,
+  };
+}
 
 export default Page;
