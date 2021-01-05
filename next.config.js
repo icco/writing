0508dbcefd34fa6b9295ecba3299cd5071e8a2dc
Nat Welch <nat@natwelch.com>
2021-01-05 02:48:24 +0000

    headers

--- next.config.js
+++ next.config.js
@@ -30,14 +30,22 @@ module.exports = {
     return [
       {
         source: "/(.*)",
-        headers: {
-          NEL: JSON.stringify({ report_to: "default", max_age: 2592000 }),
-          "Report-To": JSON.stringify({
-            group: "default",
-            max_age: 10886400,
-            endpoints: [{ url: `https://reportd.natwelch.com/report/writing` }],
-          }),
-        },
+        headers: [
+          {
+            key: "NEL",
+            value: JSON.stringify({ report_to: "default", max_age: 2592000 }),
+          },
+          {
+            key: "Report-To",
+            value: JSON.stringify({
+              group: "default",
+              max_age: 10886400,
+              endpoints: [
+                { url: `https://reportd.natwelch.com/report/writing` },
+              ],
+            }),
+          },
+        ],
       },
       {
         source: "/(.*)",
