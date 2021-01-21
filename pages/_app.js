ac76018dd22e7615287b6fe700960d282516d5d0
Nat Welch <nat@natwelch.com>
2021-01-21 13:47:36 +0000

    lint

--- pages/_app.js
+++ pages/_app.js
@@ -36,16 +36,15 @@ function Writing({ Component, pageProps }) {
 
 // Will be called once for every metric that has to be reported.
 export function reportWebVitals(metric) {
-  const body = JSON.stringify(metric)
-  const url = 'https://reportd.natwelch.com/analytics/writing'
+  const body = JSON.stringify(metric);
+  const url = "https://reportd.natwelch.com/analytics/writing";
 
   // Use `navigator.sendBeacon()` if available, falling back to `fetch()`.
   if (navigator.sendBeacon) {
-    navigator.sendBeacon(url, body)
+    navigator.sendBeacon(url, body);
   } else {
-    fetch(url, { body, method: 'POST', keepalive: true })
+    fetch(url, { body, method: "POST", keepalive: true });
   }
 }
-
 
 export default Writing;
