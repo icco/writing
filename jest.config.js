249b0de6c4a9d252b439cfaa236db77166a920fc
Nat Welch <nat@natwelch.com>
2022-12-26 20:21:44 +0000

    lib

--- jest.config.js
+++ jest.config.js
@@ -9,9 +9,8 @@ const createJestConfig = nextJest({
 const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   moduleNameMapper: {
-    // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
-
+    '^@/lib/(.*)$': '<rootDir>/lib/$1',
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
