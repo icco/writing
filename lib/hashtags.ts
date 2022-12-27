f5b76a6a25700ae58eb56b64e8498dbb2d1161ef
Nat Welch <nat@natwelch.com>
2022-12-27 04:00:58 +0000

    cleanup

--- lib/hashtags.ts
+++ lib/hashtags.ts
@@ -3,8 +3,6 @@ import {
   PhrasingContent,
   ReplaceFunction,
 } from "mdast-util-find-and-replace/lib"
-import { toString } from "mdast-util-to-string"
-import { visit } from "unist-util-visit"
 
 /**
  * Plugin to autolink references for hashtags.
@@ -15,31 +13,6 @@ export default function remarkHashtags() {
   return (tree, vfile) => {
     findAndReplace(tree, /(?:#)([a-z1-9]\d*)/gi, replaceHashtag)
 
-    visit(tree, "link", (node) => {
-      const link = parse(node)
-
-      if (!link) {
-        return
-      }
-
-      const comment = link.comment ? " (comment)" : ""
-
-      /** @type {StaticPhrasingContent[]} */
-      const children = []
-
-      if (base) {
-        children.push({ type: "text", value: base + "@" })
-      }
-
-      children.push({ type: "inlineCode", value: link.reference })
-
-      if (link.comment) {
-        children.push({ type: "text", value: comment })
-      }
-
-      node.children = children
-    })
-
     /**
      * @type {ReplaceFunction}
      * @param {string} value
@@ -47,9 +20,9 @@ export default function remarkHashtags() {
      * @param {Match} match
      */
     function replaceHashtag(
-      value,
-      no,
-      match
+      value: string,
+      no: string,
+      match: Match
     ): PhrasingContent | string | false | undefined | null {
       if (
         /\w/.test(match.input.charAt(match.index - 1)) ||
@@ -69,55 +42,5 @@ export default function remarkHashtags() {
           }
         : false
     }
-  }
-}
-
-/**
- * Parse a link and determine whether it links to GitHub.
- *
- * @param {import('mdast').Link} node
- * @returns {{user: string, project: string, page: string, reference: string, comment: boolean}|undefined}
- */
-function parse(node) {
-  const url = node.url || ""
-  const match = linkRegex.exec(url)
-
-  if (
-    // Not a proper URL.
-    !match ||
-    // Looks like formatting.
-    node.children.length !== 1 ||
-    node.children[0].type !== "text" ||
-    toString(node) !== url ||
-    // SHAs can be min 4, max 40 characters.
-    (match[3] === "commit" && (match[4].length < 4 || match[4].length > 40)) ||
-    // SHAs can be min 4, max 40 characters.
-    (match[3] === "compare" &&
-      !/^[a-f\d]{4,40}\.{3}[a-f\d]{4,40}$/.test(match[4])) ||
-    // Issues / PRs are decimal only.
-    ((match[3] === "issues" || match[3] === "pull") &&
-      /[a-f]/i.test(match[4])) ||
-    // Projects can be at most 99 characters.
-    match[2].length >= 100
-  ) {
-    return
-  }
-
-  let reference = match[4]
-
-  if (match[3] === "compare") {
-    const [base, compare] = reference.split("...")
-    reference = abbr(base) + "..." + abbr(compare)
-  } else {
-    reference = abbr(reference)
-  }
-
-  return {
-    user: match[1],
-    project: match[2],
-    page: match[3],
-    reference,
-    comment:
-      url.charAt(match[0].length) === "#" && match[0].length + 1 < url.length,
   }
 }
