c3f1b108cea0e8a2cf74e88d69fb5b3c556fab89
Nat Welch <nat@natwelch.com>
2022-12-27 04:28:50 +0000

    eh

--- lib/hashtags.ts
+++ lib/hashtags.ts
@@ -1,8 +1,5 @@
 import { findAndReplace } from "mdast-util-find-and-replace"
-import {
-  PhrasingContent,
-  ReplaceFunction,
-} from "mdast-util-find-and-replace/lib"
+import { PhrasingContent } from "mdast-util-find-and-replace/lib"
 
 /**
  * Plugin to autolink references for hashtags.
@@ -11,36 +8,20 @@ import {
  */
 export default function remarkHashtags() {
   return (tree, vfile) => {
-    findAndReplace(tree, /(?:#)([a-z1-9]\d*)/gi, replaceHashtag)
+    findAndReplace(tree, /(?:#)(\w+)/g, replaceHashtag)
 
-    /**
-     * @type {ReplaceFunction}
-     * @param {string} value
-     * @param {string} no
-     * @param {Match} match
-     */
     function replaceHashtag(
       value: string,
-      no: string,
-      match: Match
+      match: string[]
     ): PhrasingContent | string | false | undefined | null {
-      if (
-        /\w/.test(match.input.charAt(match.index - 1)) ||
-        /\w/.test(match.input.charAt(match.index + value.length))
-      ) {
-        return false
+      const url = `https://writing.natwelch.com/tag/${match}`
+
+      return {
+        type: "link",
+        title: `#${value}`,
+        url,
+        children: [{ type: "text", value }],
       }
-
-      const url = `https://writing.natwelch.com/tag/${tag}`
-
-      return url
-        ? {
-            type: "link",
-            title: null,
-            url,
-            children: [{ type: "text", value }],
-          }
-        : false
     }
   }
 }
