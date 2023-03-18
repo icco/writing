4b06e7cb7ecb0205b2d288319279345c089958e1
Nat Welch <nat@natwelch.com>
2023-03-18 21:13:02 +0000

    sure

---
+++ src/app/post/[pid]/page.tsx
@@ -0,0 +1,21 @@
+async function getData() {
+  const res = await fetch('https://api.example.com/...');
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+
+export default async function Page() {
+ // const data = await getData();
+
+  return (<article>
+    <h1>My Blog Post</h1>
+  </article>);
+}
