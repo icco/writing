1e0c2096500e22cc6f85494ee73bbca7626a51a6
Nat Welch <nat@natwelch.com>
2023-03-18 19:41:04 +0000

    fixes

--- next.config.js
+++ next.config.js
@@ -1,9 +1,42 @@
+// @ts-check
+const securityHeaders = [
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on'
+  },
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=63072000; includeSubDomains; preload'
+  },
+
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  productionBrowserSourceMaps: true,
+  poweredByHeader: false,	
   experimental: {
     appDir: true,
   },
   output: 'standalone',
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "https://natwelch.com/wiki/about",
+        permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        // Apply these headers to all routes in your application.
+        source: '/:path*',
+        headers: securityHeaders,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
