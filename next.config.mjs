2f8b67e08702f67f1c56df6d0d834e7c94f62909
Nat Welch <nat@natwelch.com>
2023-08-15 22:07:13 +0000

    headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,11 @@
 import remarkGfm from 'remark-gfm'
 import createMDX from '@next/mdx'
+import { createSecureHeaders } from "next-secure-headers";
+
+const port = process.env.PORT || 8080;
+const domain = process.env.DOMAIN || `http://localhost:${port}`;
+const graphql =
+  process.env.GRAPHQL_ORIGIN || "https://graphql.natwelch.com/graphql";
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -7,7 +13,92 @@ const nextConfig = {
   experimental: {
     mdxRs: true,
   },
-}
+  productionBrowserSourceMaps: true,
+  reactStrictMode: true,
+  trailingSlash: false,
+  env: {
+    DOMAIN: domain,
+    GRAPHQL_ORIGIN: graphql,
+    PORT: port,
+  },
+  async redirects() {
+    return [
+      {
+        source: "/about",
+        destination: "https://natwelch.com/",
+        permanent: true,
+      },
+    ];
+  },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "NEL",
+            value: JSON.stringify({ report_to: "default", max_age: 2592000 }),
+          },
+          {
+            key: "Report-To",
+            value: JSON.stringify({
+              group: "default",
+              max_age: 10886400,
+              endpoints: [
+                { url: `https://reportd.natwelch.com/report/writing` },
+              ],
+            }),
+          },
+        ],
+      },
+      {
+        source: "/(.*)",
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              // default-src 'none'
+              defaultSrc: ["'none'"],
+              // connect-src https://graphql.natwelch.com/graphql
+              connectSrc: ["https://*.natwelch.com", domain, domain.replace(/^https?/, "ws"),],
+              // font-src https://fonts.gstatic.com
+              fontSrc: ["https://fonts.gstatic.com"],
+              // img-src 'self' data: https://icco.imgix.net https://storage.googleapis.com
+              imgSrc: [
+                "'self'",
+                "data:",
+                "https://icco.imgix.net",
+                "https://storage.googleapis.com",
+                "https://*.natwelch.com",
+              ],
+              // script-src 'self' 'unsafe-inline'
+              scriptSrc: [
+                "'self'",
+                "'unsafe-inline'",
+                "'unsafe-eval'",
+                "blob:",
+                "https://*.natwelch.com",
+                domain,
+              ],
+              // style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/
+              styleSrc: [
+                "'self'",
+                "'unsafe-inline'",
+                "https://fonts.googleapis.com/",
+              ],
+              objectSrc: ["'none'"],
+              // https://developers.google.com/web/updates/2018/09/reportingapi#csp
+              reportURI: "https://reportd.natwelch.com/report/writing",
+              reportTo: "default",
+            },
+            reportOnly: true,
+          },
+          referrerPolicy: "strict-origin-when-cross-origin",
+          expectCT: true,
+        }),
+      },
+    ];
+  },
+};
 
 const withMDX = createMDX({
   options: {
