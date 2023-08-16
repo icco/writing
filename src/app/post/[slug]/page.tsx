c4649120bc16596e29b399f41680ddf065f60397
Nat Welch <nat@natwelch.com>
2023-08-16 01:50:43 +0000

    simplify

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -1,11 +1,3 @@
 export default function Page({ params }: { params: { slug: string } }) {
   return <div>My Post: {params.slug}</div>
 }
-
-export async function generateStaticParams() {
-  const posts = await fetch('https://.../posts').then((res) => res.json())
- 
-  return posts.map((post: { slug: string }) => ({
-    slug: post.slug,
-  }))
-}
