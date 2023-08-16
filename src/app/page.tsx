2a098bbe415304cf05a5d1bdc6f10eee53bbac1c
Nat Welch <nat@natwelch.com>
2023-08-16 02:10:10 +0000

    sorting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ function PostCard(post: Post) {
 }
 
 export default function Home() {
-  const posts = allPosts.sort((b, a) => compareDesc(new Date(a.datetime), new Date(b.datetime)))
+  const posts = allPosts.sort((a, b) => compareDesc(new Date(a.datetime), new Date(b.datetime)))
 
   return (
     <div className="mx-auto max-w-xl py-8">
