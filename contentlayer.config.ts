c4b502090e956e16f519cb43f12ead9d33df4689
Nat Welch <nat@natwelch.com>
2023-08-16 01:48:37 +0000

    Revert "fixes"
    This reverts commit 61bdf35c4fe07e8c5a1f1b7d23abce955301facf.

--- contentlayer.config.ts
+++ contentlayer.config.ts
@@ -4,11 +4,11 @@ export const Post = defineDocumentType(() => ({
   name: 'Post',
   filePathPattern: `**/*.md`,
   fields: {
-    datetime: { type: 'date', required: true },
-    draft: { type: 'boolean', required: false, default: false },
-    id: { type: 'number', required: true },
-    permalink: { type: 'string', required: true },
     title: { type: 'string', required: true },
+    datetime: { type: 'date', required: true },
+    id: { type: 'string', required: true },
+    draft: { type: 'boolean', required: false, default: false },
+    permalink: { type: 'string', required: true },
   },
   computedFields: {
     url: { type: 'string', resolve: (post) => post.permalink },
