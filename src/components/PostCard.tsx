766eb46ce68f83ca8f01ac4b0a8ce144c8259807
Nat Welch <nat@natwelch.com>
2023-08-18 21:02:57 +0000

    format

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -5,32 +5,32 @@ import { Post } from "contentlayer/generated"
 
 export function PostCard(post: Post) {
   return (
-    <div className="mb-8">
-      <h1 className="mb-1 text-xl">
+    <div className="mb-5">
+      <div
+        className="inline-block	pr-2 text-xs text-muted"
+      >
+        #{post.id}
+      </div>
+      <h1 className="mb-1 text-4xl hover:text-link">
         <Link
           href={post.url}
         >
           {post.title}
         </Link>
       </h1>
-      <div>
-        <div
-          className="inline-block	pr-2 mb-2 text-xs text-gray-600"
-        >#{post.id}</div>
-        <time
-          dateTime={post.datetime}
-          className="inline-block	mb-2 text-xs text-gray-600"
-        >
-          {format(parseISO(post.datetime), "LLLL d, yyyy")}
-        </time>
-      </div>
+      <time
+        dateTime={post.datetime}
+        className="inline-block	align-right mb-2 text-xs text-muted"
+      >
+        {format(parseISO(post.datetime), "LLLL d, yyyy")}
+      </time>
 
-      <div className="mb-2 text-sm text-gray-600 dark:text-gray-400">
+      <div className="mb-2 text-sm text-gray-600 dark:text-muted">
         {post.tags.map((tag: string) => {
           return (
             <Link
               href={`/tag/${tag}`}
-              className="inline-block	pr-1 mb-2 text-xs"
+              className="inline-block	pr-1 mb-2 text-xs hover:text-link"
               key={tag}>
               #{tag}
             </Link>
