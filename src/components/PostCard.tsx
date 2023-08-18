10ec9eb76d45edc02dfffdb919e65bc907939509
Nat Welch <nat@natwelch.com>
2023-08-18 20:22:13 +0000

    post card

--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -6,25 +6,33 @@ import { Post } from "contentlayer/generated"
 export function PostCard(post: Post) {
   return (
     <div className="mb-8">
-      <h2 className="mb-1 text-xl">
+      <h1 className="mb-1 text-xl">
         <Link
           href={post.url}
-          className="text-blue-700 hover:text-blue-900 dark:text-blue-400"
         >
           {post.title}
         </Link>
-      </h2>
-      <time
-        dateTime={post.datetime}
-        className="mb-2 block text-xs text-gray-600"
-      >
-        {format(parseISO(post.datetime), "LLLL d, yyyy")}
-      </time>
+      </h1>
+      <div>
+        <div
+          className="inline-block	pr-2 mb-2 text-xs text-gray-600"
+        >#{post.id}</div>
+        <time
+          dateTime={post.datetime}
+          className="inline-block	mb-2 text-xs text-gray-600"
+        >
+          {format(parseISO(post.datetime), "LLLL d, yyyy")}
+        </time>
+      </div>
+
       <div className="mb-2 text-sm text-gray-600 dark:text-gray-400">
         {post.tags.map((tag: string) => {
           return (
-            <Link href={`/tag/${tag}`} key={tag}>
-              {tag}
+            <Link
+              href={`/tag/${tag}`}
+              className="inline-block	pr-1 mb-2 text-xs"
+              key={tag}>
+              #{tag}
             </Link>
           )
         })}
