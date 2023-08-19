edd7c4a88b4d895fc1edc01516d0afcebe7f16d5
Nat Welch <nat@natwelch.com>
2023-08-19 01:27:29 +0000

    rss feeds

---
+++ src/app/feed.atom/page.tsx
@@ -0,0 +1,11 @@
+import generateFeed from "@/lib/feed"
+import { allPosts } from "contentlayer/generated"
+
+export async function GET() {
+  const feed = await generateFeed(allPosts)
+  return new Response(feed.atom1(), {
+    headers: {
+      "Content-Type": "application/atom+xml",
+    },
+  });
+}
