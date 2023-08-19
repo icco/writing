c9bb47808c51d224e91bee7b3ff19d38a8da713a
Nat Welch <nat@natwelch.com>
2023-08-19 01:32:45 +0000

    alternates

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -33,12 +33,18 @@ export const generateMetadata = ({ params }: { params: { slug: string } }) => {
       locale: "en_US",
       type: "article",
     },
-    canonical: post.url,
     viewport: {
       width: "device-width",
       initialScale: 1,
       maximumScale: 1,
     },
+    alternates: {
+      canonical: post.url,
+      types: {
+        'application/rss+xml': 'https://writing.natwelch.com/feed.rss',
+        'application/atom+xml': 'https://writing.natwelch.com/feed.rss',
+      },
+    }
   }
 }
 
