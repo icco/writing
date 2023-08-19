1a16ca85539d91fba3774ff2504f74cf82ffc528
Nat Welch <nat@natwelch.com>
2023-08-19 01:42:30 +0000

    lint

--- src/app/feed.rss/route.tsx
+++ src/app/feed.rss/route.tsx
@@ -2,7 +2,7 @@ import generateFeed from "@/lib/feed"
 
 import { allPosts } from "contentlayer/generated"
 
-export async function GET(request: Request) {
+export async function GET() {
   const feed = await generateFeed(allPosts)
   return new Response(feed.rss2(), {
     headers: {
