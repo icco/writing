6f29eaacaa45c3eb469c8a93417e7a7aee3927f6
Nat Welch <nat@natwelch.com>
2023-08-26 04:19:30 +0000

    err

---
+++ src/app/post/[slug]/error.tsx
@@ -0,0 +1,15 @@
+'use client' // Error components must be Client Components
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div>
+      <h2>{error.name}</h2>
+    </div>
+  )
+}
