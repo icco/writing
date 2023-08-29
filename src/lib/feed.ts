6a016ae7a6a94d41cd00eeba170f192c265ff56e
Nat Welch <nat@natwelch.com>
2023-08-29 20:04:22 +0000

    feedlink

--- src/lib/feed.ts
+++ src/lib/feed.ts
@@ -4,13 +4,14 @@ import { Post } from "contentlayer/generated"
 
 export default async function generateFeed(posts: Post[]) {
   const feed = new Feed({
-    id: "NatNatNat",
+    id: "https://writing.natwelch.com/",
     title: "Nat? Nat. Nat!",
     favicon: "https://writing.natwelch.com/favicon.ico",
     description: "Nat Welch's blog about random stuff.",
     link: "https://writing.natwelch.com/",
     feedLinks: {
       atom: "https://writing.natwelch.com/feed.atom",
+      rss: "https://writing.natwelch.com/feed.rss",
     },
     author: {
       name: "Nat Welch",
@@ -34,6 +35,7 @@ export default async function generateFeed(posts: Post[]) {
             link: "https://natwelch.com",
           },
         ],
+        content: p.body.raw,
       })
     })
   } catch (err) {
