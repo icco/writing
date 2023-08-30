54036cd409063b2561dcc6461e32300e27956668
Nat Welch <nat@natwelch.com>
2023-08-30 00:52:10 +0000

    fix posts

--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,12 +1,12 @@
-import { compareDesc } from "date-fns"
+import { compareDesc, isFuture } from "date-fns"
 
 import { allPosts } from "contentlayer/generated"
 
 export default function publishedPosts() {
   const posts = allPosts
-    .sort((a, b) => compareDesc(new Date(a.datetime), new Date(b.datetime)))
     .filter((post) => !post.draft)
-    .filter((post) => post.datetime <= new Date().toISOString())
+    .filter((post) => !isFuture(new Date(post.datetime)))
+    .sort((a, b) => compareDesc(new Date(a.datetime), new Date(b.datetime)))
 
   return posts
 }
