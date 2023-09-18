23b5e1b2145037824114996ba6daa95a5b2db832
Nat Welch <nat@natwelch.com>
2023-09-18 02:32:19 +0000

    fix feed html

--- src/lib/feed.ts
+++ src/lib/feed.ts
@@ -1,6 +1,7 @@
 import { Feed } from "feed"
 
 import { Post } from "contentlayer/generated"
+import { format } from "date-fns"
 
 export default async function generateFeed(posts: Post[]) {
   const feed = new Feed({
@@ -19,7 +20,7 @@ export default async function generateFeed(posts: Post[]) {
       link: "https://natwelch.com",
     },
     language: "en",
-    copyright: "2022",
+    copyright: `2011 - ${format(new Date(), "yyyy")} Nat Welch. All rights reserved.`,
   })
 
   try {
@@ -28,6 +29,7 @@ export default async function generateFeed(posts: Post[]) {
         title: p.title,
         link: `https://writing.natwelch.com/post/${p.id}`,
         date: new Date(p.datetime),
+        category: p.tags.map((t: string) => ({ name: t, term: t, })),
         author: [
           {
             name: "Nat Welch",
@@ -35,7 +37,7 @@ export default async function generateFeed(posts: Post[]) {
             link: "https://natwelch.com",
           },
         ],
-        content: p.body.raw,
+        content: `Due to a rendering bug, this post is not available in the feed. Please visit <a href="https://writing.natwelch.com/post/${p.id}">https://writing.natwelch.com/post/${p.id}</a> to read it.`,
       })
     })
   } catch (err) {
