b2be6bd741431bc1ad52d44a30cf94c0a3349726
Nat Welch <nat@natwelch.com>
2023-10-28 20:04:05 +0000

    lint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,9 +6,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require("@tailwindcss/typography")],
   theme: {
     colors: {
       nord0: "#2e3440",
