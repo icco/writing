faa625308e972124bcbd5fe1907c90c2fd84ee3d
Nat Welch <nat@natwelch.com>
2023-10-28 19:57:24 +0000

    typography

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,9 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+  ],
   theme: {
     colors: {
       nord0: "#2e3440",
