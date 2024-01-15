84b73598c2b5cf5d38ecc24ba3955ed610c46278
Nat Welch <nat@natwelch.com>
2024-01-15 22:53:37 +0000

    netter

--- src/app/post/[slug]/error.tsx
+++ src/app/post/[slug]/error.tsx
@@ -7,8 +7,38 @@ export default function Error({
   error: Error & { digest?: string }
   reset: () => void
 }) {
-  let code = 404
-  let message = error.message
+  let code = 0
+  let message = ""
+
+  console.log(error.message)
+
+  switch (error.digest) {
+    case 'NEXT_NOT_FOUND':
+      code = 404
+      message = 'Not Found'
+      break
+    case 'NEXT_UNAUTHORIZED':
+      code = 401
+      message = 'Unauthorized'
+      break
+    case 'NEXT_FORBIDDEN':
+      code = 403
+      message = 'Forbidden'
+      break
+    case 'NEXT_BAD_REQUEST':
+      code = 400
+      message = 'Bad Request'
+      break
+    case 'NEXT_UNAVAILABLE':
+      code = 503
+      message = 'Service Unavailable'
+      break
+    default:
+      code = 500
+      message = 'Internal Server Error'
+      break
+  }
+
   return (
     <>
       <div className="flex flex-col items-center justify-center">
