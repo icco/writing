dd8336301e4b5403f92a726e99189e19dbe34bcc
Nat Welch <nat@natwelch.com>
2024-01-15 04:33:18 +0000

    sure

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -7,7 +7,7 @@ function ThemeToggle() {
 
   return (
     <button
-      className="mt-16 px-4 py-2 text-white dark:text-black bg-black dark:bg-white font-semibold rounded-md"
+      className=""
       onClick={() => {
         setTheme(theme === "light" ? "dark" : "light")
       }}
