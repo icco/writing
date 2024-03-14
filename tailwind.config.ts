0ca93f9e46055e0787a56b1e974b222c43b58c59
Nat Welch <nat@natwelch.com>
2024-03-14 14:43:53 +0000

    lint

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,7 @@ import type { Config } from "tailwindcss"
 const config: Config = {
   future: {
     removeDeprecatedGapUtilities: true,
-    purgeLayersByDefault: true
+    purgeLayersByDefault: true,
   },
   darkMode: "class",
   content: [
@@ -11,36 +11,7 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('tailwind-nord'),
-  ],
-  theme: {
-    colors: {
-      // nord0: "#2e3440",
-      // nord1: "#3b4252",
-      // nord2: "#434c5e",
-      // nord3: "#4c566a",
-      // nord4: "#d8dee9",
-      // nord5: "#e5e9f0",
-      // nord6: "#eceff4",
-      // nord7: "#8fbcbb",
-      // nord8: "#88c0d0",
-      // nord9: "#81a1c1",
-      // nord10: "#5e81ac",
-      // nord11: "#bf616a",
-      // nord12: "#d08770",
-      // nord13: "#ebcb8b",
-      // nord14: "#a3be8c",
-      // nord15: "#b48ead",
-      // background: "#eceff4",
-      // text: "#2e3440",
-      // border: "#454e61",
-      // primary: "#88C0D0",
-      // accent: "#d8dee9",
-      // muted: "#394050",
-      // link: "#5E81AC",
-    },
-  },
+  plugins: [require("@tailwindcss/typography"), require("tailwind-nord")],
+  theme: {},
 }
 export default config
