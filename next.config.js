b7f479b02ec403cccfe6126f636127f2bd65bbd4
Nat Welch <nat@natwelch.com>
2024-03-21 11:55:42 +0000

    one change

--- next.config.js
+++ next.config.js
@@ -65,6 +65,7 @@ const nextConfig = {
                 "https://*.natwelch.com",
                 domain,
                 domain.replace(/^https?/, "ws"),
+                "https://browser.sentry-cdn.com",
               ],
               // font-src 'self' https://fonts.gstatic.com
               fontSrc: ["'self'", "https://fonts.gstatic.com"],
@@ -84,6 +85,7 @@ const nextConfig = {
                 "blob:",
                 "https://*.natwelch.com",
                 "https://snippet.meticulous.ai",
+                "https://browser.sentry-cdn.com",
                 domain,
               ],
               // style-src 'self' 'unsafe-inline' https://fonts.googleapis.com/
