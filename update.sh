939385bc18d70cc34893b5a652726013faf2cc33
Nat Welch <nat@natwelch.com>
2024-04-13 16:29:57 +0000

    chore: update.sh fix

--- update.sh
+++ update.sh
@@ -7,20 +7,15 @@ rm -rf package-lock.json dist node_modules
 yarn 
 yarn upgrade 
 git add package* yarn.lock
-git ci -m 'chore(deps): yarn upgrade'
+git diff --quiet --staged || git ci -m 'chore(deps): yarn upgrade'
 
 yarn run lint 
 git add src
-
-if [[ "" != "$(git status -uno --porcelain)" ]]; then
-  git ci -m 'chore: lint'
-fi
+git diff --quiet --staged || git ci -m 'chore: lint'
 
 yarn run build
 
 git add public
-if [[ "" != "$(git status -uno --porcelain)" ]]; then
-  git ci -m 'chore: update build artifacts'
-fi
+git diff --quiet --staged || git ci -m 'chore: update build artifacts'
 
 git push -u
