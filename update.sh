cd5530f24a49248049beab9b276ee38e8ff27851
Nat Welch <nat@natwelch.com>
2024-04-15 19:39:11 +0000

    fix alias

--- update.sh
+++ update.sh
@@ -7,15 +7,15 @@ rm -rf package-lock.json dist node_modules .next .contentlayer
 yarn 
 yarn upgrade 
 git add package* yarn.lock
-git diff --quiet --staged || git ci -m 'chore(deps): yarn upgrade'
+git diff --quiet --staged || git commit -m 'chore(deps): yarn upgrade'
 
 yarn run lint 
 git add src
-git diff --quiet --staged || git ci -m 'chore: lint'
+git diff --quiet --staged || git commit -m 'chore: lint'
 
 yarn run build
 
 git add public
-git diff --quiet --staged || git ci -m 'chore: update build artifacts'
+git diff --quiet --staged || git commit -m 'chore: update build artifacts'
 
 git push -u
