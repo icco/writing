99e6b90df9bf6bdf37e6b3713a865bcd88216d42
Nat Welch <nat@natwelch.com>
2024-10-03 13:09:54 +0000

    progress

--- src/components/Pre.tsx
+++ src/components/Pre.tsx
@@ -1,7 +1,28 @@
-export default function Pre({ children }) {
-  return (
-    <pre className="p-4 overflow-x-auto bg-gray-800 text-white rounded-md">
-      {children}
-    </pre>
-  )
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism";
+
+type PreProps = {
+  inline?: boolean;
+  className?: string;
+  children: React.ReactNode | string | string[];
 }
+
+const Pre: React.FC<PreProps> = ({ inline, className, children, ...props }) => {
+  const hasLang = /language-(\w+)/.exec(className || "");
+  return !inline && hasLang ? (
+    <SyntaxHighlighter
+      style={oneDark}
+      language={hasLang[1]}
+      PreTag="div"
+      className="mockup-code scrollbar-thin scrollbar-track-base-content/5 scrollbar-thumb-base-content/40 scrollbar-track-rounded-md scrollbar-thumb-rounded"
+      showLineNumbers={true}
+      useInlineStyles={true}
+    >
+      {children?.toString().replace(/\n$/, "")}
+    </SyntaxHighlighter>
+  ) : (
+    <pre className={className} {...props}>{children}</pre>
+  );
+}
+
+export default Pre;
