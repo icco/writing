388c3123c7b3e92f10f51d5bed7c34f93d74baaf
Nat Welch <nat@natwelch.com>
2024-12-16 01:41:45 +0000

    lint

--- src/components/PhotoGrid.tsx
+++ src/components/PhotoGrid.tsx
@@ -9,6 +9,7 @@ const PhotoGrid: React.FC<PhotoGridProps> = ({ urls }) => {
     <div className="columns-2 md:columns-3 gap-2 not-prose">
       {urls.map((url, index) => (
         <a href={url} key={index}>
+          {/* eslint-disable-next-line @next/next/no-img-element */}
           <img
             src={url + "?w=500"}
             alt={`Grid item ${index + 1}`}
