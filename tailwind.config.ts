040ca3682c2b46065a4c7df70f02e7c1e8d13351
Nat Welch <nat@natwelch.com>
2025-02-03 03:53:43 +0000

    from photos

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,14 +1,11 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
+/* eslint-disable @typescript-eslint/no-require-imports */
 const config: Config = {
-  future: {},
-  darkMode: "class",
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  // eslint-disable-next-line @typescript-eslint/no-var-requires,  @typescript-eslint/no-require-imports
-  plugins: [require("@tailwindcss/typography")],
-}
-export default config
+};
+export default config;
