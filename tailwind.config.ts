1f9db44788b3113994ab0dea9e61ac2f5a8281a6
Nat Welch <nat@natwelch.com>
2025-02-04 03:00:29 +0000

    whee

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,8 +2,6 @@ import type { Config } from "tailwindcss"
 
 /* eslint-disable @typescript-eslint/no-require-imports */
 const config: Config = {
-  content: [
-    "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 }
 export default config
