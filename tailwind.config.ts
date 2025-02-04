66ee757816e05e798bae02fab940a523e4084c07
Nat Welch <nat@natwelch.com>
2025-02-04 03:04:15 +0000

    narp

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,7 +1,7 @@
 import type { Config } from "tailwindcss"
 
-/* eslint-disable @typescript-eslint/no-require-imports */
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
 }
+
 export default config
