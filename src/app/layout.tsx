00ba412e4ebf5d7a273c8850da86e48f5ace05cf
Nat Welch <nat@natwelch.com>
2025-02-04 03:46:58 +0000

    hrm

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import "./globals.css"
-import "@wooorm/starry-night/style/dimmed" // For syntax highlighting
+// import "@wooorm/starry-night/style/dimmed" // For syntax highlighting
 
 import type { Metadata, Viewport } from "next"
 import { Roboto, Roboto_Mono, Roboto_Slab } from "next/font/google"
