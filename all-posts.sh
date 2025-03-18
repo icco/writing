3b62c6aa641e4b7ee5ecbe292427e951db70c48b
Nat Welch <nat@natwelch.com>
2025-03-18 17:43:14 +0000

    attempt array

--- all-posts.sh
+++ all-posts.sh
@@ -2,4 +2,4 @@
 
 for i in $(ls ./posts | sort -n); do
   [ $(yq --front-matter="extract" -P .draft ./posts/$i) = false ] && echo \"$( echo $i | sed 's/.md$//g' | sed 's/^/writing.natwelch.com\/post\//')\"
-done | jq -c --slurp .
+done | jq -c --slurp . | jq -r '.[]'
