30190dd6e4ff17be98fa3411427a2eec83b01ea3
Nat Welch <nat@natwelch.com>
2025-03-18 12:29:45 +0000

    script

--- all-posts.sh
+++ all-posts.sh
@@ -1,7 +1,5 @@
 #! /bin/zsh
 
-posts=$(ls ./posts | sort -n | sed 's/.md$//g' | sed 's/^/writing.natwelch.com\/post\//')
-
-for i in $posts; do
-  echo $i
-done
+for i in $(ls ./posts | sort -n); do
+  [ $(yq --front-matter="extract" -P .draft ./posts/$i) = false ] && echo \"$( echo $i | sed 's/.md$//g' | sed 's/^/writing.natwelch.com\/post\//')\"
+done | jq -c --slurp .
