88fa54c30812823c77c859f3562cac17778ff6e9
Nat Welch <nat@natwelch.com>
2025-03-20 13:56:14 +0000

    fix: changes

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -1,13 +1,20 @@
 import { draftMode } from "next/headers"
-import { redirect } from "next/navigation"
+import { notFound, redirect } from "next/navigation"
 
 import { getPostBySlug } from "@/lib/posts"
+
+import { allPosts } from "contentlayer/generated"
 
 export async function GET(request: Request) {
   // Parse query string parameters
   const { searchParams } = new URL(request.url)
   const secret = searchParams.get("secret")
   const slug = searchParams.get("slug")
+
+  console.log(
+    "Available posts:",
+    allPosts.map((p) => ({ id: p.id, title: p.title }))
+  )
 
   // Check the secret and next parameters
   // This secret should only be known to this route handler and the CMS
@@ -19,18 +26,22 @@ export async function GET(request: Request) {
     return new Response("Missing slug", { status: 400 })
   }
 
-  const post = getPostBySlug(slug)
+  try {
+    const post = getPostBySlug(slug)
+    // If the slug doesn't exist prevent draft mode from being enabled
+    if (!post) {
+      return new Response("Invalid slug", { status: 401 })
+    }
 
-  // If the slug doesn't exist prevent draft mode from being enabled
-  if (!post) {
-    return new Response("Invalid slug", { status: 401 })
+    // Enable Draft Mode by setting the cookie
+    const dm = await draftMode()
+    dm.enable()
+
+    // Redirect to the path from the fetched post
+    // We don't redirect to searchParams.slug as that might lead to open redirect vulnerabilities
+    redirect(post.permalink)
+  } catch (error) {
+    console.error("Error fetching post:", error)
+    notFound()
   }
-
-  // Enable Draft Mode by setting the cookie
-  const dm = await draftMode()
-  dm.enable()
-
-  // Redirect to the path from the fetched post
-  // We don't redirect to searchParams.slug as that might lead to open redirect vulnerabilities
-  redirect(post.permalink)
 }
--- src/lib/posts.ts
+++ src/lib/posts.ts
@@ -1,5 +1,4 @@
 import { compareDesc, isFuture } from "date-fns"
-import { notFound } from "next/navigation"
 
 import { allPosts, Post } from "contentlayer/generated"
 
@@ -30,7 +29,7 @@ export function getPostBySlug(slug: string): Post {
   const slugNumber = parseInt(slug)
   const post: Post | undefined = allPosts.find((post) => post.id === slugNumber)
   if (!post) {
-    notFound()
+    throw new Error(`Post with slug ${slug} not found`)
   }
 
   return post
