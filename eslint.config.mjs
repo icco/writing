c703f19a959a4ed1d0c2348bffb2f8dcc2317513
Nat Welch <nat@natwelch.com>
2025-09-28 14:14:40 +0000

    fix: better lint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,73 +1,24 @@
-import path from "node:path"
-import { fileURLToPath } from "node:url"
-
-import { fixupConfigRules, fixupPluginRules } from "@eslint/compat"
-import { FlatCompat } from "@eslint/eslintrc"
-import js from "@eslint/js"
-import tsParser from "@typescript-eslint/parser"
-import { defineConfig } from "eslint/config"
-import _import from "eslint-plugin-import"
-import simpleImportSort from "eslint-plugin-simple-import-sort"
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+import js from '@eslint/js'
+import { FlatCompat } from '@eslint/eslintrc'
+ 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
   recommendedConfig: js.configs.recommended,
-  allConfig: js.configs.all,
 })
-
-export default defineConfig([
-  {
-    extends: fixupConfigRules(
-      compat.extends(
-        "eslint:recommended",
-        "next/core-web-vitals",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:import/recommended",
-        "plugin:import/typescript",
-        "plugin:prettier/recommended",
-        "prettier"
-      )
-    ),
-
-    plugins: {
-      import: fixupPluginRules(_import),
-      "simple-import-sort": simpleImportSort,
-    },
-
-    languageOptions: {
-      parser: tsParser,
-      ecmaVersion: 5,
-      sourceType: "script",
-
-      parserOptions: {
-        ecmaFeatures: {
-          jsx: true,
-        },
-      },
-    },
-
+ 
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      'eslint:recommended',
+      'next',
+      'next/core-web-vitals',
+      'next/typescript',
+    ],
     rules: {
-      "import/no-unresolved": "off",
-      "import/first": "error",
-      "import/newline-after-import": "error",
-      "import/no-duplicates": "error",
-      "simple-import-sort/exports": "error",
-      "@typescript-eslint/triple-slash-reference": "off",
-      "simple-import-sort/imports": [
-        "error",
-        {
-          groups: [
-            ["^\\u0000"],
-            ["^node:"],
-            ["^@?\\w"],
-            ["^"],
-            ["^(contentlayer/generated)"],
-            ["^\\."],
-          ],
-        },
-      ],
+      '@typescript-eslint/triple-slash-reference': 'off',
     },
-  },
-])
+  }),
+]
+ 
+export default eslintConfig
