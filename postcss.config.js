474f7363ebbb4d0048b1fbed3259ae353ae8b1e3
Nat Welch <nat@natwelch.com>
2025-11-06 19:32:53 +0000

    eh

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,5 @@
-import preserveProperty from "./postcss-preserve-property.js"
-
-export default {
+module.exports = {
   plugins: {
     "@tailwindcss/postcss": {},
-    preserveProperty: preserveProperty(),
-    "postcss-preset-env": {
-      features: {
-        "custom-properties": true,
-      },
-    },
   },
 }
