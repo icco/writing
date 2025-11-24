fc9e0a55b41ac892572dec4db76ee0623deb29ec
Nat Welch <nat@natwelch.com>
2025-11-24 02:03:10 +0000

    more aliases

--- src/app/tags/page.tsx
+++ src/app/tags/page.tsx
@@ -1,12 +1,12 @@
-import { allTags, Tag } from "@/components/Tag"
+import { allTagsWithCounts, Tag } from "@/components/Tag"
 
 const TagList = () => {
   return (
     <>
       <h1 className="my-8 text-center text-4xl font-bold">Tags</h1>
       <div className="mx-auto max-w-3xl px-8 py-7">
-        {allTags().map((tag) => (
-          <Tag key={tag} tag={tag} className="p-4 text-xl" />
+        {allTagsWithCounts().map(({ tag, count }) => (
+          <Tag key={tag} tag={tag} count={count} className="p-4 text-xl" />
         ))}
       </div>
     </>
